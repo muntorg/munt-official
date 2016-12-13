6d4c4d044d06e379597e2e0187371c4072907159
Malcolm MacLeod <mmacleod@webmail.co.za>
2016-12-13 10:58:23 +0200

    Fix an issue with upgrading encrypted 1.5.5 wallets from GuldenD. Because GuldenD has no UI the password prompt didn't show, log a message telling people how to work around this case.

--- src/noui.cpp
+++ src/noui.cpp
@@ -8,6 +8,10 @@
 #include "ui_interface.h"
 #include "util.h"
 
+#ifdef ENABLE_WALLET
+#include "wallet/wallet.h"
+#endif
+
 #include <cstdio>
 #include <stdint.h>
 #include <string>
@@ -49,10 +53,30 @@ static void noui_InitMessage(const std::string& message)
     LogPrintf("init message: %s\n", message);
 }
 
+#ifdef ENABLE_WALLET
+static void NotifyRequestUnlockS(CWallet* wallet, std::string reason)
+{
+    SecureString passwd = GetArg("-unlockpasswd", "").c_str();
+    if (!passwd.empty())
+    {
+        if (!wallet->Unlock(passwd))
+        {
+            fprintf(stderr, "Wallet requested unlock but -unlockpasswd was invalid - please unlock via RPC or in the case of an upgrade temporarily set -unlockpasswd in Gulden.conf: reason [%s]\n", reason.c_str());
+            return;
+        }
+    }
+    fprintf(stderr, "Wallet requested unlock but could not unlock - please unlock via RPC or in the case of an upgrade temporarily set -unlockpasswd in Gulden.conf: reason [%s]\n", reason.c_str());
+}
+#endif
+
 void noui_connect()
 {
     // Connect bitcoind signal handlers
     uiInterface.ThreadSafeMessageBox.connect(noui_ThreadSafeMessageBox);
     uiInterface.ThreadSafeQuestion.connect(noui_ThreadSafeQuestion);
     uiInterface.InitMessage.connect(noui_InitMessage);
+    
+    #ifdef ENABLE_WALLET
+    uiInterface.RequestUnlock.connect(boost::bind(NotifyRequestUnlockS, _1, _2));
+    #endif
 }
