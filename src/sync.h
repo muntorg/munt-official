6650633712ecb17623644f35df3eeb8922dd8244
Malcolm MacLeod <mmacleod@webmail.co.za>
2017-10-16 10:23:00 +0200

    Minor ammendement to new DS_LOCK2 macro, it was not accounting for the fact that some code passes NULL for the mutex (e.g. when running with no wallet) - which was leading to 'deadlock' style freezes as a result when users use the -disablewallet flag.

--- src/sync.h
+++ src/sync.h
@@ -176,6 +176,9 @@ typedef CMutexLock<CCriticalSection> CCriticalBlock;
 #define LOCK2(cs1, cs2) CCriticalBlock criticalblock1(cs1, #cs1, __FILE__, __LINE__), criticalblock2(cs2, #cs2, __FILE__, __LINE__)
 #define TRY_LOCK(cs, name) CCriticalBlock name(cs, #cs, __FILE__, __LINE__, true)
 
+template<class T> bool IsNullMutex(T& m) { return false; }
+template<class T> bool IsNullMutex(T* m) { if (m == NULL) { return true; } return false; }
+
 // Achieve LOCK2, using a loop and TRY_LOCK to allow for an opportunity for any deadlocks to resolve themselves (at the cost of performance)
 // For now we favour stability over performance, over time we should convert all DS_LOCK2 into LOCK2 one at a time with strict testing.
 #define DS_LOCK2(cs1, cs2)                                                                                     \
@@ -185,7 +188,7 @@ while(true)                                                                     
 {                                                                                                              \
     criticalblock1 = std::shared_ptr<CCriticalBlock>(new CCriticalBlock(cs1, #cs1, __FILE__, __LINE__, true)); \
     criticalblock2 = std::shared_ptr<CCriticalBlock>(new CCriticalBlock(cs2, #cs2, __FILE__, __LINE__, true)); \
-    if (!(*criticalblock1) || !(*criticalblock2))                                                              \
+    if ((!(*criticalblock1) && !IsNullMutex(cs1)) || (!(*criticalblock2) && !IsNullMutex(cs2)))                \
     {                                                                                                          \
         criticalblock1 = nullptr;                                                                              \
         criticalblock2 = nullptr;                                                                              \
