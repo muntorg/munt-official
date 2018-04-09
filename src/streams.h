b80d9adb24d9e9dc1d1bfbf3d4c1bb49e8f30543
Willem de Jonge <willem@isnapp.nl>
2018-04-09 14:31:47 +0200

    Fix -reindex
    The reindex reads all blocks again. While doing this the validation will also read disk blocks, this moves the same filepointer as the reading by reindex is using. So before doing the next read a seek is needed to be in the correct position.

--- src/streams.h
+++ src/streams.h
@@ -603,6 +603,10 @@ protected:
             readNow = nAvail;
         if (readNow == 0)
             return false;
+        if (fseek(src, nSrcPos, SEEK_SET) != 0)
+        {
+            throw std::ios_base::failure("CBufferedFile::Fill fseek failed");
+        }
         size_t nBytes = fread((void*)&vchBuf[pos], 1, readNow, src);
         if (nBytes == 0) {
             throw std::ios_base::failure(feof(src) ? "CBufferedFile::Fill: end of file" : "CBufferedFile::Fill: fread failed");
