5f7a1419fb8be987d78097f0662a438adf739117
Malcolm MacLeod <malcolm@gulden.com>
2018-04-28 22:25:23 +0200

    Bump copyright years.

--- src/Gulden/Common/diff_common.h
+++ src/Gulden/Common/diff_common.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2017 The Gulden developers
+// Copyright (c) 2015-2018 The Gulden developers
 // Authored by: Malcolm MacLeod (mmacleod@webmail.co.za)
 // Distributed under the GULDEN software license, see the accompanying
 // file COPYING
