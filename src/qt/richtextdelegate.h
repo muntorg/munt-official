a23b73d4215c1d79d6ad451ab6976cc9c9d59683
Malcolm MacLeod <malcolm@gulden.com>
2018-05-12 11:33:19 +0200

    Add missing files for richtextdelegate.

---
+++ src/qt/richtextdelegate.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2016-2018 The Gulden developers
+// Authored by: Malcolm MacLeod (mmacleod@webmail.co.za)
+// Distributed under the GULDEN software license, see the accompanying
+// file COPYING
+
+#ifndef GULDEN_QT_RICHTEXTDELEGATE_H
+#define GULDEN_QT_RICHTEXTDELEGATE_H
+
+#include <QStyledItemDelegate>
+
+class RichTextDelegate : public QStyledItemDelegate
+{
+protected:
+    void paint ( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const;
+    QSize sizeHint ( const QStyleOptionViewItem & option, const QModelIndex & index ) const;
+};
+
+#endif // GULDEN_QT_RICHTEXTDELEGATE_H
