e3692a6377f82a83664ad7b28e3154a14ab813f4
Malcolm MacLeod <malcolm@gulden.com>
2018-05-28 23:22:00 +0200

    Another (hopefully final this time) attempt at getting ICU cross compiling to behave on all possible platforms.

--- depends/packages/icu.mk
+++ depends/packages/icu.mk
@@ -6,24 +6,32 @@ $(package)_sha256_hash=0eb46ba3746a9c2092c8ad347a29b1a1b4941144772d13a88667a7b11
 $(package)_build_subdir=source
 
 
+$(package)_patch_xlocale = sed -i 's|xlocale[.]h|locale.h|' i18n/digitlst.cpp
+$(package)_reverse_patch_xlocale_linux = true
+$(package)_reverse_patch_xlocale_mingw32 = true
+$(package)_reverse_patch_xlocale_darwin = sed -i 's|locale[.]h|xlocale.h|' i18n/digitlst.cpp
+$(package)_reverse_patch_xlocale =  $($(package)_reverse_patch_xlocale_$(host_os))
+
+
 define $(package)_set_vars
 $(package)_config_opts=
 endef
 
 define $(package)_config_cmds
-  find . -name *.cpp | xargs sed -i 's|xlocale[.]h|locale.h|' && \
+  $($(package)_patch_xlocale) && \
   mkdir native && cd native && \
-	PATH="${HOST_NATIVE_PATH}:${PATH}" ../configure && \
-  PATH="${HOST_NATIVE_PATH}:${PATH}"  make -j 10 && cd .. && \
-  $($(package)_autoconf) --with-cross-build=$($(package)_build_dir)/native --prefix=$(host_prefix) --enable-extras=no \
+  PATH=$(or $(HOST_NATIVE_PATH),${PATH}) ../configure && \
+  PATH=$(or $(HOST_NATIVE_PATH),${PATH}) make -j 10 && cd .. && \
+  $($(package)_reverse_patch_xlocale) && \
+  PATH=${PATH} $($(package)_autoconf) --with-cross-build=$($(package)_build_dir)/native --prefix=$(host_prefix) --enable-extras=no \
   --enable-strict=no -enable-shared=yes --enable-tests=no \
-  --enable-samples=no --enable-dyload=no --enable-tools=no
+  --enable-samples=no --enable-dyload=no --enable-tools=no CXXFLAGS="--std=c++11"
 endef
 
 define $(package)_build_cmds
   sed -i.old 's|gCLocale = _create_locale(LC_ALL, "C");|gCLocale = NULL;|' i18n/digitlst.cpp && \
   sed -i.old "s|freelocale(gCLocale);||" i18n/digitlst.cpp && \
-  $(MAKE)
+  PATH=${PATH} $(MAKE)
 endef
 
 define $(package)_stage_cmds
