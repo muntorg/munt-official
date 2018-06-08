5cbdad3b1edfb2d1671d0da0c7a5b3f01732d475
Malcolm MacLeod <malcolm@gulden.com>
2018-06-08 11:20:57 +0200

    Minor unit test name change and dead code removal.

--- src/test/test_gulden_main.cpp
+++ src/test/test_gulden_main.cpp
@@ -2,25 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#define BOOST_TEST_MODULE Bitcoin Test Suite
+#define BOOST_TEST_MODULE Gulden unit tests
 
 #include "net.h"
 
 #include <boost/test/unit_test.hpp>
-
-/*std::unique_ptr<CConnman> g_connman;
-
-void Shutdown(void* parg)
-{
-  exit(EXIT_SUCCESS);
-}
-
-void StartShutdown()
-{
-  exit(EXIT_SUCCESS);
-}
-
-bool ShutdownRequested()
-{
-  return false;
-}*/
