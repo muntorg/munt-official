9a5caefbab3a497f22bf3a9af8cf94e8e88dacac
Malcolm MacLeod <malcolm@gulden.com>
2018-06-28 07:24:57 +0200

    Fix some potential deadlocks in code

--- src/wallet/wallet_keypool.cpp
+++ src/wallet/wallet_keypool.cpp
@@ -64,7 +64,7 @@ int CWallet::TopUpKeyPool(unsigned int nTargetKeypoolSize, unsigned int nMaxNewA
     unsigned int nNew = 0;
     bool bAnyNonHDAccountsLockedAndRequireKeys = false;
 
-    LOCK(cs_wallet);
+    LOCK2(cs_main, cs_wallet);
 
     CWalletDB walletdb(*dbw);
 
@@ -106,6 +106,7 @@ int CWallet::TopUpKeyPool(unsigned int nTargetKeypoolSize, unsigned int nMaxNewA
                     if (!account->IsHD() && account->IsLocked())
                     {
                         bAnyNonHDAccountsLockedAndRequireKeys = true;
+                        break;
                     }
                     else
                     {
@@ -133,7 +134,7 @@ void CWallet::ReserveKeyFromKeyPool(int64_t& nIndex, CKeyPool& keypoolentry, CAc
     nIndex = -1;
     keypoolentry.vchPubKey = CPubKey();
     {
-        LOCK(cs_wallet);
+        LOCK2(cs_main, cs_wallet);
 
         if (!IsLocked())
             TopUpKeyPool(1, 0, forAccount);//Only assign the bare minimum here, let the background thread do the rest.
