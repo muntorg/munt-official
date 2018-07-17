410b0ed7119300d6fd09887fd4997b3b6f08fb53
Malcolm MacLeod <malcolm@gulden.com>
2018-07-17 15:30:50 +0200

    Update latest DNS seeders

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -143,10 +143,10 @@ public:
         assert(genesis.hashMerkleRoot == uint256S("0x4bed0bcb3e6097445ae68d455137625bb66f0e7ba06d9db80290bf72e3d6dcf8"));
 
         vSeeds.push_back(CDNSSeedData("seed 0",  "seed.gulden.com", false));
-        vSeeds.push_back(CDNSSeedData("seed 1",  "amsterdam.gulden.com"));
-//        vSeeds.push_back(CDNSSeedData("seed 2",  "seed.gulden.network"));
-        vSeeds.push_back(CDNSSeedData("seed 3",  "rotterdam.gulden.network", false));
-//        vSeeds.push_back(CDNSSeedData("seed 4",  "seed.gulden.blue"));
+        vSeeds.push_back(CDNSSeedData("seed 1",  "amsterdam.gulden.com", false));
+        vSeeds.push_back(CDNSSeedData("seed 2",  "rotterdam.gulden.network", false));
+        //vSeeds.push_back(CDNSSeedData("seed 3",  "seed.gulden.network"));
+        //vSeeds.push_back(CDNSSeedData("seed 4",  "seed.gulden.blue"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,38);// 'G'
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,98);// 'g'
