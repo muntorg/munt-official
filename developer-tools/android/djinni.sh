f3f34d7e4a3d47070748318b21d67e507e5af6f6
Malcolm MacLeod <malcolm@gulden.com>
2018-09-10 21:08:03 +0200

    Add basic developer_tools script to generate bindings using djinni

---
+++ developer-tools/android/djinni.sh
@@ -0,0 +1,14 @@
+#bin/bash
+
+djinni/djinni/src/run \
+   --java-out ./src/frontend/android/unity_wallet/app/src/main/java/com/gulden/jniunifiedbackend/ \
+   --java-package com.gulden.jniunifiedbackend \
+   --ident-java-field mFooBar \
+   --cpp-out src/unity/djinni/cpp/ \
+   --jni-out src/unity/djinni/jni/ \
+   --ident-jni-class NativeFooBar \
+   --ident-jni-file NativeFooBar \
+   --objc-out src/unity/djinni/objc/ \
+   --objc-type-prefix DB \
+   --objcpp-out src/unity/djinni/objc/ \
+   --idl src/unity/libunity.djinni
