fa044b10c7f28a927b64374ffbeaf2151260884d
Malcolm MacLeod <malcolm@gulden.com>
2018-09-10 20:31:40 +0200

    Patch depends building to work with NDK - this may require additional changes to make it work for regular builds again...

--- depends/hosts/default.mk
+++ depends/hosts/default.mk
@@ -2,8 +2,8 @@ ifneq ($(host),$(build))
 host_toolchain:=$(host)-
 endif
 
-default_host_CC = $(host_toolchain)gcc
-default_host_CXX = $(host_toolchain)g++
+default_host_CC = $(host_toolchain)clang
+default_host_CXX = $(host_toolchain)clang++
 default_host_AR = $(host_toolchain)ar
 default_host_RANLIB = $(host_toolchain)ranlib
 default_host_STRIP = $(host_toolchain)strip
--- depends/hosts/linux.mk
+++ depends/hosts/linux.mk
@@ -10,19 +10,19 @@ linux_debug_CXXFLAGS=$(linux_debug_CFLAGS)
 linux_debug_CPPFLAGS=-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
 
 ifeq (86,$(findstring 86,$(build_arch)))
-i686_linux_CC=gcc -m32
-i686_linux_CXX=g++ -m32
-i686_linux_AR=ar
-i686_linux_RANLIB=ranlib
-i686_linux_NM=nm
-i686_linux_STRIP=strip
+i686_linux_CC=$(default_host_CC) -m32
+i686_linux_CXX=$(default_host_CXX) -m32
+i686_linux_AR=$(default_host_AR)
+i686_linux_RANLIB=$(default_host_RANLIB)
+i686_linux_NM=$(default_host_NM)
+i686_linux_STRIP=$(default_host_STRIP)
 
-x86_64_linux_CC=gcc -m64
-x86_64_linux_CXX=g++ -m64
-x86_64_linux_AR=ar
-x86_64_linux_RANLIB=ranlib
-x86_64_linux_NM=nm
-x86_64_linux_STRIP=strip
+x86_64_linux_CC=$(default_host_CC) -m64
+x86_64_linux_CXX=$(default_host_CXX) -m64
+x86_64_linux_AR=$(default_host_AR)
+x86_64_linux_RANLIB=$(default_host_RANLIB)
+x86_64_linux_NM=$(default_host_NM)
+x86_64_linux_STRIP=$(default_host_STRIP)
 else
 i686_linux_CC=$(default_host_CC) -m32
 i686_linux_CXX=$(default_host_CXX) -m32
