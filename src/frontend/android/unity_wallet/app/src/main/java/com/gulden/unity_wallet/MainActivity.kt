66773c7f109240df9f49186c038a96ed8d62b72c
Malcolm MacLeod <malcolm@gulden.com>
2018-09-12 15:32:31 +0200

    android: Implement basic fragment switching and placeholder layout for "send fragment"

--- src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/MainActivity.kt
+++ src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/MainActivity.kt
@@ -1,34 +1,65 @@
 package com.gulden.unity_wallet
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import com.gulden.jniunifiedbackend.GuldenUnifiedBackend
 import com.gulden.jniunifiedbackend.GuldenUnifiedFrontendImpl
+import com.gulden.unity_wallet.SendFragment.OnFragmentInteractionListener
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity() {
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+    beginTransaction().func().commit()
+}
+
+fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int){
+    supportFragmentManager.inTransaction { add(frameId, fragment) }
+}
+
+fun AppCompatActivity.replaceFragment(fragment: Fragment, frameId: Int) {
+    supportFragmentManager.inTransaction{replace(frameId, fragment)}
+}
+
+class MainActivity : AppCompatActivity(), OnFragmentInteractionListener, ReceiveFragment.OnFragmentInteractionListener, TransactionFragment.OnFragmentInteractionListener, SettingsFragment.OnFragmentInteractionListener  {
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented")
+    }
+
+    private val sendFragment : Fragment = SendFragment();
+    private val receiveFragment : Fragment = ReceiveFragment();
+    private val transactionFragment : Fragment = TransactionFragment();
+    private val settingsFragment : Fragment = SettingsFragment();
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_send -> {
+                replaceFragment(sendFragment, R.id.mainLayout)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_receive -> {
+                replaceFragment(receiveFragment, R.id.mainLayout)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_transactions -> {
+                replaceFragment(transactionFragment, R.id.mainLayout)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_settings -> {
+                replaceFragment(settingsFragment, R.id.mainLayout)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
-
     private val coreSignals: GuldenUnifiedFrontendImpl?=GuldenUnifiedFrontendImpl()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +75,8 @@ class MainActivity : AppCompatActivity() {
 
         syncProgress.setMax(1000000);
         syncProgress.setProgress(0);
+
+        addFragment(sendFragment, R.id.mainLayout)
     }
 
     public fun updateSyncProgressPercent(percent : Float)
--- src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/SendFragment.kt
+++ src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/SendFragment.kt
@@ -17,13 +17,13 @@ private const val ARG_PARAM2 = "param2"
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [SendFrament.OnFragmentInteractionListener] interface
+ * [SendFragment.OnFragmentInteractionListener] interface
  * to handle interaction events.
- * Use the [SendFrament.newInstance] factory method to
+ * Use the [SendFragment.newInstance] factory method to
  * create an instance of this fragment.
  *
  */
-class SendFrament : Fragment() {
+class SendFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -40,7 +40,7 @@ class SendFrament : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_frament, container, false)
+        return inflater.inflate(R.layout.fragment_send, container, false)
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -85,12 +85,12 @@ class SendFrament : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SendFrament.
+         * @return A new instance of fragment SendFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-                SendFrament().apply {
+                SendFragment().apply {
                     arguments = Bundle().apply {
                         putString(ARG_PARAM1, param1)
                         putString(ARG_PARAM2, param2)
