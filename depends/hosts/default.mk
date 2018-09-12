e02577c36276333f28b356445e5f5e753866914f
Malcolm MacLeod <malcolm@gulden.com>
2018-09-12 11:04:14 +0200

    Possible fix for travis build.

--- depends/hosts/default.mk
+++ depends/hosts/default.mk
@@ -5,22 +5,62 @@ endif
 ifeq ($(CC),)
   default_host_CC = $(host_toolchain)gcc
 else
-  default_host_CC=$(CC)
+  default_host_CC= $(CC)
 endif
 
 ifeq ($(CXX),)
   default_host_CXX = $(host_toolchain)g++
 else
-  default_host_CXX =$(CXX)
+  default_host_CXX = $(CXX)
 endif
 
-default_host_AR = $(host_toolchain)ar
-default_host_RANLIB = $(host_toolchain)ranlib
-default_host_STRIP = $(host_toolchain)strip
-default_host_LIBTOOL = $(host_toolchain)libtool
+ifeq ($(AR),)
+  default_host_AR = $(host_toolchain)ar
+  ifeq (, $(shell which $(default_host_AR)))
+     default_host_AR = ar
+  endif
+else
+  default_host_AR = $(AR)
+endif
+
+ifeq ($(RANLIB),)
+  default_host_RANLIB = $(host_toolchain)ranlib
+  ifeq (, $(shell which $(default_host_RANLIB)))
+     default_host_RANLIB = ranlib
+  endif
+else
+  default_host_RANLIB = $(RANLIB)
+endif
+
+ifeq ($(STRIP),)
+  default_host_STRIP = $(host_toolchain)strip
+  ifeq (, $(shell which $(default_host_STRIP)))
+     default_host_STRIP = strip
+  endif
+else
+  default_host_STRIP = $(RANLIB)
+endif
+
+ifeq ($(LIBTOOL),)
+  default_host_LIBTOOL = $(host_toolchain)libtool
+  ifeq (, $(shell which $(default_host_LIBTOOL)))
+     default_host_LIBTOOL = libtool
+  endif
+else
+  default_host_LIBTOOL = $(LIBTOOL)
+endif
+
+ifeq ($(NM),)
+  default_host_NM = $(host_toolchain)nm
+  ifeq (, $(shell which $(default_host_NM)))
+     default_host_NM = nm
+  endif
+else
+  default_host_NM = $(NM)
+endif
+
 default_host_INSTALL_NAME_TOOL = $(host_toolchain)install_name_tool
 default_host_OTOOL = $(host_toolchain)otool
-default_host_NM = $(host_toolchain)nm
 
 define add_host_tool_func
 $(host_os)_$1?=$$(default_host_$1)
