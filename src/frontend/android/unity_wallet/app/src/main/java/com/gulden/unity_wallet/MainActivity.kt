9e46965ddcb9b7a4fe617f33ea713ff044474853
Malcolm MacLeod <malcolm@gulden.com>
2018-09-12 11:13:40 +0200

    android: Add placeholder fragments for each of our four tabs

--- src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/MainActivity.kt
+++ src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import com.gulden.jniunifiedbackend.GuldenUnifiedBackend
-import com.gulden.jniunifiedbackend.GuldenUnifiedFrontend
 import com.gulden.jniunifiedbackend.GuldenUnifiedFrontendImpl
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.concurrent.thread
@@ -43,12 +42,12 @@ class MainActivity : AppCompatActivity() {
         }
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
-        progressBar2.setMax(1000000);
-        progressBar2.setProgress(0);
+        syncProgress.setMax(1000000);
+        syncProgress.setProgress(0);
     }
 
     public fun updateSyncProgressPercent(percent : Float)
     {
-        progressBar2.setProgress((1000000 * (percent/100)).toInt());
+        syncProgress.setProgress((1000000 * (percent/100)).toInt());
     }
 }
