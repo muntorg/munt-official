f955500b582eee165f457a7941ce417be0454ee9
Malcolm MacLeod <malcolm@gulden.com>
2018-10-03 10:56:07 +0200

    unity: Add copyright notices to several source files

--- src/unity/libinit.h
+++ src/unity/libinit.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2018 The Gulden developers
+// Authored by: Malcolm MacLeod (mmacleod@webmail.co.za)
+// Distributed under the GULDEN software license, see the accompanying
+// file COPYING
+
 #ifndef GULDEN_LIB_INIT
 #define GULDEN_LIB_INIT
 
