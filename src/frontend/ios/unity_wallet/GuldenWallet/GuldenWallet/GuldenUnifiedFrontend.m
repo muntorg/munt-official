c46d6cd2841830b55732166f14f9f986e361d050
Willem de Jonge <willem@isnapp.nl>
2018-10-12 22:39:18 +0200

    Remove iOS wallet project
    The iOS wallet will not be in the core repository

--- src/frontend/ios/unity_wallet/GuldenWallet/GuldenWallet/GuldenUnifiedFrontend.m
+++
@@ -1,9 +0,0 @@
-//
-//  GuldenUnifiedFrontend.m
-//  GuldenWallet
-//
-//  Created by Willem de Jonge on 05/10/2018.
-//  Copyright © 2018 Gulden.com BV. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
