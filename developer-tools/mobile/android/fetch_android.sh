4b6eeb757d0ec149552f76e840b24ff030dee8df
Malcolm MacLeod <malcolm@Malcolms-MacBook-Pro.local>
2018-10-17 21:47:55 +0200

    unity: Update fetch_android script for macOS and to use latest sdk release.

--- developer-tools/mobile/android/fetch_android.sh
+++ developer-tools/mobile/android/fetch_android.sh
@@ -2,19 +2,21 @@
 set -e
 set -x
 
-export NDK_VERSION=android-ndk-r18-beta2
-export NDK_FILENAME=${NDK_VERSION}-linux-x86_64.zip
+export NDK_VERSION=android-ndk-r18b
+export NDK_FILENAME=${NDK_VERSION}-darwin-x86_64.zip
 
 #sha256_file=5dfbbdc2d3ba859fed90d0e978af87c71a91a5be1f6e1c40ba697503d48ccecd
 
-#mkdir /opt/android-ndk-gulden | true
-#cd /opt/android-ndk-gulden && curl -sSO https://dl.google.com/android/repository/${NDK_FILENAME} &> /dev/null
+mkdir ./developer-tools/android-ndk-gulden | true
+cd ./developer-tools/android-ndk-gulden
+#curl -sSO https://dl.google.com/android/repository/${NDK_FILENAME} &> /dev/null
 #echo "${sha256_file}  ${NDK_FILENAME}" | shasum -a 256 --check
-#unzip -qq ${NDK_FILENAME} &> /dev/null
-#rm ${NDK_FILENAME}
+unzip -qq ${NDK_FILENAME} &> /dev/null
+rm ${NDK_FILENAME}
+cd -
 
-/opt/android-ndk-gulden/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=arm-linux-androideabi-clang --install-dir=/opt/android-ndk-gulden/arm-linux-androideabi-clang
-/opt/android-ndk-gulden/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=x86-clang --install-dir=/opt/android-ndk-gulden/x86-clang
-/opt/android-ndk-gulden/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=x86_64-clang --install-dir=/opt/android-ndk-gulden/x86_64-clang
-/opt/android-ndk-gulden/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=aarch64-linux-android-clang --install-dir=/opt/android-ndk-gulden/aarch64-linux-android-clang
+${PWD}/developer-tools/android-ndk-gulden/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=arm-linux-androideabi-clang --install-dir=${PWD}/developer-tools/android-ndk-gulden/arm-linux-androideabi-clang
+${PWD}/developer-tools/android-ndk-gulden/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=x86-clang --install-dir=${PWD}/developer-tools/android-ndk-gulden/x86-clang
+${PWD}/developer-tools/android-ndk-gulden/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=x86_64-clang --install-dir=${PWD}/developer-tools/android-ndk-gulden/x86_64-clang
+${PWD}/developer-tools/android-ndk-gulden/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=aarch64-linux-android-clang --install-dir=${PWD}/developer-tools/android-ndk-gulden/aarch64-linux-android-clang
 
