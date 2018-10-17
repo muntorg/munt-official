6dea497a23dfdaf93f2dff6e0a964e2657587c18
Malcolm MacLeod <malcolm@gulden.com>
2018-10-17 22:13:13 +0200

    unity: Update fetch_android script with host platform detection for linux/darwin

--- developer-tools/mobile/android/fetch_android.sh
+++ developer-tools/mobile/android/fetch_android.sh
@@ -2,14 +2,24 @@
 set -e
 set -x
 
+
+case "$OSTYPE" in
+  darwin*)
+      PLATFORM=darwin
+      ;;
+  *)
+      PLATFORM=linux
+      ;;
+esac
+
 export NDK_VERSION=android-ndk-r18b
-export NDK_FILENAME=${NDK_VERSION}-darwin-x86_64.zip
+export NDK_FILENAME=${NDK_VERSION}-${PLATFORM}-x86_64.zip
 
 #sha256_file=5dfbbdc2d3ba859fed90d0e978af87c71a91a5be1f6e1c40ba697503d48ccecd
 
 mkdir ./developer-tools/android-ndk-gulden | true
 cd ./developer-tools/android-ndk-gulden
-#curl -sSO https://dl.google.com/android/repository/${NDK_FILENAME} &> /dev/null
+curl -sSO https://dl.google.com/android/repository/${NDK_FILENAME} &> /dev/null
 #echo "${sha256_file}  ${NDK_FILENAME}" | shasum -a 256 --check
 unzip -qq ${NDK_FILENAME} &> /dev/null
 rm ${NDK_FILENAME}
