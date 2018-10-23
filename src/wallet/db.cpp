5f3941b5f74bb5aeca04963deee95acfa9f15f41
Willem de Jonge <willem@isnapp.nl>
2018-10-23 14:50:54 +0200

    Revert "WORKAROUND FIX THIS! Because Berkley DB was built without locking."
    This reverts commit 43ccc195f3fd53379aa1f89dc028486aaae5c2a0.

--- src/wallet/db.cpp
+++ src/wallet/db.cpp
@@ -94,7 +94,7 @@ bool CDBEnv::Open(const fs::path& pathIn)
     dbenv->log_set_config(DB_LOG_AUTO_REMOVE, 1);
     int ret = dbenv->open(strPath.c_str(),
                          DB_CREATE |
-                             //DB_INIT_LOCK |
+                             DB_INIT_LOCK |
                              DB_INIT_LOG |
                              DB_INIT_MPOOL |
                              DB_INIT_TXN |
