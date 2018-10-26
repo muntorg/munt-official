4e373c3838800d2572341fe302a2b563422a650b
Willem de Jonge <willem@isnapp.nl>
2018-10-26 21:04:08 +0200

    Add DBAddressRecord.h to GuldenCore framework

--- src/unity/ios/gulden_static/GuldenCore/GuldenCore.h
+++ src/unity/ios/gulden_static/GuldenCore/GuldenCore.h
@@ -16,6 +16,7 @@ FOUNDATION_EXPORT const unsigned char gulden_staticVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <gulden_static/PublicHeader.h>
 
+#import "DBAddressRecord.h"
 #import "DBGuldenUnifiedBackend.h"
 #import "DBGuldenUnifiedFrontend.h"
 #import "DBBalanceRecord.h"
