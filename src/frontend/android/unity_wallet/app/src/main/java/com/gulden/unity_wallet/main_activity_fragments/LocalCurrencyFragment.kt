28a9662cefbba0a1b58ddc01a8030cfc281806b8
Malcolm MacLeod <malcolm@gulden.com>
2019-02-05 15:57:26 +0200

    ANDROID: UI: Implement basics of "local currency" selection screen.
    Still some things to correct, doesn't display exchange rates, displays incorrect in preference fragment and other minor improvements still to be done. WIP.

---
+++ src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/main_activity_fragments/LocalCurrencyFragment.kt
@@ -0,0 +1,63 @@
+// Copyright (c) 2018 The Gulden developers
+// Authored by: Malcolm MacLeod (mmacleod@webmail.co.za)
+// Distributed under the GULDEN software license, see the accompanying
+// file COPYING
+
+package com.gulden.unity_wallet.main_activity_fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import com.gulden.unity_wallet.R
+import com.gulden.unity_wallet.Currencies
+import com.gulden.unity_wallet.ui.LocalCurrenciesAdapter
+import kotlinx.android.synthetic.main.fragment_local_currencies.*
+
+
+class LocalCurrencyFragment : androidx.fragment.app.Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+        return inflater.inflate(R.layout.fragment_local_currencies, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?)
+    {
+        super.onActivityCreated(savedInstanceState)
+
+        val adapter = LocalCurrenciesAdapter(this.context!!, Currencies.knownCurrencies)
+        currenciesList.adapter = adapter
+        currenciesList.setOnItemClickListener { parent, _, position, _ ->
+            adapter.setSelectedPosition(position)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    override fun onAttach(context: Context)
+    {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener)
+        {
+            listener = context
+        }
+        else
+        {
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    private var listener: OnFragmentInteractionListener? = null
+    interface OnFragmentInteractionListener
+    {
+        fun onFragmentInteraction(uri: Uri)
+    }
+}
---
+++ src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/ui/LocalCurrenciesAdapter.kt
@@ -0,0 +1,54 @@
+// Copyright (c) 2018 The Gulden developers
+// Authored by: Malcolm MacLeod (mmacleod@webmail.co.za)
+// Distributed under the GULDEN software license, see the accompanying
+// file COPYING
+
+package com.gulden.unity_wallet.ui
+
+import android.content.Context
+import android.preference.PreferenceManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.gulden.unity_wallet.*
+import com.gulden.unity_wallet.Currency
+import kotlinx.android.synthetic.main.local_currency_list_item.view.*
+import java.util.*
+
+class LocalCurrenciesAdapter(private val context: Context, private val dataSource: TreeMap<String, Currency>) : BaseAdapter() {
+    private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+
+    override fun getItem(position: Int) : Currency?
+    {
+        return dataSource.values.toTypedArray()[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        val currencyRecord = getItem(position) as Currency
+        val rowView = inflater.inflate(R.layout.local_currency_list_item, parent, false)
+        rowView.textViewCurrencyName.text = currencyRecord.name
+        rowView.textViewCurrencyCode.text = currencyRecord.code
+        rowView.imageViewCurrencySelected.visibility = if (currencyRecord.code == localCurrency.code)
+        {
+            View.VISIBLE
+        }
+        else
+        {
+            View.GONE
+        }
+        return rowView
+    }
+
+    fun setSelectedPosition(position: Int) = PreferenceManager.getDefaultSharedPreferences(AppContext.instance).edit().putString("preference_local_currency", getItem(position)?.code).apply()
+}
+
