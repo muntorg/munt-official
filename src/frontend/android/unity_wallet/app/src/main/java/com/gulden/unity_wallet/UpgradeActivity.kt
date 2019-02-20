2294f79a4b71dee4b561d22b3a02603524beb475
Malcolm MacLeod <malcolm@gulden.com>
2019-02-20 22:57:25 +0200

    ANDROID: Rework update activity to use unity backend instead of guldenj

--- src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/UpgradeActivity.kt
+++ src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/UpgradeActivity.kt
@@ -1,5 +1,5 @@
 // Copyright (c) 2018 The Gulden developers
-// Authored by: Willem de Jonge (willem@isnapp.nl)
+// Authored by: Willem de Jonge (willem@isnapp.nl), Malcolm MacLeod (mmacleod@gmx.com)
 // Distributed under the GULDEN software license, see the accompanying
 // file COPYING
 
@@ -9,32 +9,24 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.gulden.jniunifiedbackend.GuldenUnifiedBackend
+import com.gulden.jniunifiedbackend.LegacyWalletResult
 import com.gulden.unity_wallet.Constants.OLD_WALLET_PROTOBUF_FILENAME
 import kotlinx.android.synthetic.main.upgrade_password.view.*
-import org.guldenj.core.Base58
-import org.guldenj.crypto.ChildNumber
-import org.guldenj.wallet.DeterministicKeyChain
-import org.guldenj.wallet.DeterministicKeyChain.*
-import org.guldenj.wallet.Wallet
-import org.guldenj.wallet.WalletProtobufSerializer
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.appcompat.v7.Appcompat
 import org.jetbrains.anko.design.longSnackbar
 import java.io.File
-import java.io.FileInputStream
-import kotlin.math.max
-import kotlin.math.min
+import kotlin.concurrent.thread
 
 private const val TAG = "upgrade-activity"
 
 class UpgradeActivity : AppCompatActivity(), UnityCore.Observer
 {
-    private var wallet: Wallet? = null
+    private var haveExistingWalletFile = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,8 +34,6 @@ class UpgradeActivity : AppCompatActivity(), UnityCore.Observer
         setContentView(R.layout.activity_upgrade)
 
         UnityCore.instance.addObserver(this, fun (callback:() -> Unit) { runOnUiThread { callback() }})
-
-        wallet = walletFromProtobufFile(getFileStreamPath(OLD_WALLET_PROTOBUF_FILENAME))
     }
 
     override fun onDestroy() {
@@ -72,92 +62,91 @@ class UpgradeActivity : AppCompatActivity(), UnityCore.Observer
         return true
     }
 
-    fun onUpgrade(view: View)
+    fun onUpgradeWithPassword(view : View, password : String)
     {
-        wallet?.let { mywallet ->
-            if (mywallet.isEncrypted) {
-                val myCustomView = layoutInflater.inflate(R.layout.upgrade_password, null)
-                val builder = alert(Appcompat) {
-                    title = getString(R.string.upgrade_enter_password)
-                    customView = myCustomView
-                    positiveButton("OK") {
-                        try {
-                            mywallet.decrypt(myCustomView.password.text)
-                            extractAndInitRecovery(mywallet)
-                        }
-                        catch (exception: Throwable) {
-                            view.longSnackbar(getString(R.string.upgrade_wrong_password).format(exception.message!!))
-                        }
-                    }
-                }
-                val dialog = builder.build()
-                dialog.setOnShowListener {
-                    myCustomView.password.requestFocus()
-                    val imm = application.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.showSoftInput(myCustomView.password, InputMethodManager.SHOW_IMPLICIT)
-                }
-                dialog.show()
+        thread(true)
+        {
+            val result = GuldenUnifiedBackend.isValidAndroidLegacyProtoWallet(filesDir.toString() + File.separator + OLD_WALLET_PROTOBUF_FILENAME, password)
+            if (result == LegacyWalletResult.PASSWORD_INVALID)
+            {
+                this.runOnUiThread { view.longSnackbar(getString(R.string.upgrade_wrong_password)) }
+            }
+            else if (result == LegacyWalletResult.INVALID_OR_CORRUPT)
+            {
+                this.runOnUiThread { view.longSnackbar("Unable to upgrade old wallet, contact support for assistance.") }
             }
             else
-                extractAndInitRecovery(mywallet)
-        }
-    }
-
-    private fun extractAndInitRecovery(wallet: Wallet) {
-        assert(!wallet.isEncrypted)
-        val recoveryMnemonic = wallet.keyChainSeed.mnemonicCode?.joinToString(" ")
-        // first attempt to extract mnemonic seed
-        if (recoveryMnemonic != null) {
-            // use transaction time and last processed block to determine birth time
-            var recoveryTime = wallet.lastBlockSeenTimeSecs
-            for (tx in wallet.transactionsByTime) {
-                recoveryTime = min(recoveryTime, tx.updateTime.time / 1000)
+            {
+                GuldenUnifiedBackend.InitWalletFromAndroidLegacyProtoWallet(filesDir.toString() + File.separator + OLD_WALLET_PROTOBUF_FILENAME, password);
+                this.runOnUiThread { view.longSnackbar("Wallet upgrade in progress") }
             }
-            recoveryTime = max(0, recoveryTime  - 24 * 60 * 60)
-
-            val recoveryPhrase = GuldenUnifiedBackend.ComposeRecoveryPhrase(recoveryMnemonic, recoveryTime)
-            assert(GuldenUnifiedBackend.IsValidRecoveryPhrase(recoveryPhrase))
-
-            Log.i(TAG, "old wallet mnemonic extracted")
-
-            GuldenUnifiedBackend.InitWalletFromRecoveryPhrase(recoveryPhrase)
-        }
-        else { // mnemonic extraction failed, we should have a linked wallet
-            val key = wallet.getKeyByPath(DeterministicKeyChain.DESKTOP_SYNC_ZERO_PATH)
-            val timeString = Base58.encode(key.creationTimeSeconds.toString().toByteArray())
-            val pvk = Base58.encode(key.privKeyBytes33).substring(1)
-            val chaincode= Base58.encode(key.chainCode)
-            val linkedUri = "guldensync:%s-%s:%s;unused_payout".format(pvk,chaincode,timeString)
-            GuldenUnifiedBackend.InitWalletLinkedFromURI(linkedUri)
         }
     }
 
-    private fun walletFromProtobufFile(walletFile: File): Wallet {
-        Log.i(TAG, "start loading old protobuf wallet")
-        assert(walletFile.exists()) // require existence has already been established
-        val walletStream = FileInputStream(walletFile)
-        val wallet = WalletProtobufSerializer().readWallet(walletStream)
-        Log.i(TAG, "finished loading protobuf wallet")
-        return wallet
+    private var processingUpgrade = false;
+    fun onUpgrade(view: View)
+    {
+        if (processingUpgrade)
+            return
+        processingUpgrade = true;
+
+        thread(true)
+        {
+            when (GuldenUnifiedBackend.isValidAndroidLegacyProtoWallet(filesDir.toString() + File.separator + OLD_WALLET_PROTOBUF_FILENAME, ""))
+            {
+                LegacyWalletResult.ENCRYPTED_PASSWORD_REQUIRED ->
+                {
+                    this.runOnUiThread {
+                        val upgradeDialogView = layoutInflater.inflate(R.layout.upgrade_password, null)
+                        val builder = alert(Appcompat) {
+                            title = getString(R.string.upgrade_enter_password)
+                            customView = upgradeDialogView
+                            positiveButton("OK") {
+                                onUpgradeWithPassword(view, upgradeDialogView.password.text.toString())
+                            }
+                        }
+                        val dialog = builder.build()
+                        dialog.setOnShowListener {
+                            upgradeDialogView.password.requestFocus()
+                            val imm = application.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                            imm.showSoftInput(upgradeDialogView.password, InputMethodManager.SHOW_IMPLICIT)
+                        }
+                        dialog.show()
+                    }
+                }
+                LegacyWalletResult.VALID ->
+                {
+                    GuldenUnifiedBackend.InitWalletFromAndroidLegacyProtoWallet(filesDir.toString() + File.separator + OLD_WALLET_PROTOBUF_FILENAME, "");
+                }
+                else ->
+                {
+                    this.runOnUiThread { view.longSnackbar("Unable to upgrade old wallet, contact support for assistance.") }
+                }
+            }
+        }
+
+        processingUpgrade = false
     }
 
     @Suppress("UNUSED_PARAMETER")
     fun onStartFresh(view: View)
     {
-        alert(Appcompat, getString(R.string.upgrade_start_fresh_desription), getString(R.string.upgrade_start_fresh_title)) {
-            positiveButton("Start fresh") {
+        alert(Appcompat, getString(R.string.upgrade_start_fresh_desription), getString(R.string.upgrade_start_fresh_title))
+        {
+            positiveButton("Start fresh")
+            {
                 gotoActivity(WelcomeActivity::class.java)
             }
-            negativeButton("Cancel") {
+            negativeButton("Cancel")
+            {
             }
         }.show()
     }
 
     private fun gotoActivity(cls: Class<*> )
     {
-            val intent = Intent(this, cls)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            startActivity(intent)
-            finish()
+        val intent = Intent(this, cls)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
     }
 }
