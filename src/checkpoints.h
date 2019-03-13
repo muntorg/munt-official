9a1e4b465ccce979bebb40bd44e3bd5e78d935cd
Malcolm MacLeod <malcolm@gulden.com>
2019-03-13 15:50:18 +0200

    CORE: Introduce function LastCheckpointBeforeBlock

--- src/checkpoints.h
+++ src/checkpoints.h
@@ -1,6 +1,13 @@
 // Copyright (c) 2009-2016 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+//
+// File contains modifications by: The Gulden developers
+// All modifications:
+// Copyright (c) 2019 The Gulden developers
+// Authored by: Malcolm MacLeod (mmacleod@gmx.com)
+// Distributed under the GULDEN software license, see the accompanying
+// file COPYING
 
 #ifndef GULDEN_CHECKPOINTS_H
 #define GULDEN_CHECKPOINTS_H
@@ -22,9 +29,13 @@ CBlockIndex* GetLastCheckpointIndex();
 //! Height of last checkpoint in Params()
 int LastCheckPointHeight();
 
+/** Last checkpoint with block height before or at blockHeight.
+    Returns height of the checkpoint, or -1 iof there is none. */
+int LastCheckpointBeforeBlock(uint64_t blockHeight);
+
 /** Last checkpoint with timestamp before or at atTime.
     Returns height of the checkpoint, or -1 iof there is none. */
-int LastCheckpointAt(int64_t atTime, CheckPointEntry& entry);
+int LastCheckpointBeforeTime(uint64_t atTime);
 
 } //namespace Checkpoints
 
