13ca20ea2ef2db6307d48f5947290095ad8ac706
Malcolm MacLeod <malcolm@gulden.com>
2019-03-19 23:29:54 +0200

    MISC: Minor spelling correction

--- src/validation/validation.h
+++ src/validation/validation.h
@@ -640,7 +640,7 @@ void SetMaxSPVPruneHeight(int height);
 /**
  * Compute new filter ranges, stored in partialChain.blockFilterRanges.
  * Filter range starts at nWalletBirthBlockHard and moves nWalletBirthBlockSoft down to the
- * first block that might contatin data for the activeWallet.
+ * first block that might contain data for the activeWallet.
  */
 void ComputeNewFilterRanges(uint64_t nWalletBirthBlockHard, uint64_t& nWalletBirthBlockSoft);
 
