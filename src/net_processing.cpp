bc6fc914290a3ec9e71ecec6bc12ca2192514afc
Malcolm MacLeod <malcolm@gulden.com>
2019-08-19 12:44:01 +0200

    MISC: Minor code comment correction.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -593,7 +593,7 @@ void FindNextBlocksToDownload(NodeId nodeid, unsigned int count, std::vector<con
                 CBlockIndex* lastCheckpoint = Checkpoints::GetLastCheckpoint(Params().Checkpoints());
 
                 // If we were stuck on the wrong side of a fork (due to not updating)
-                // And have now updated to a nwer version, then this block may be valid on the newer version despite being marked invalid previously by the outdated version
+                // And have now updated to a newer version, then this block may be valid on the newer version despite being marked invalid previously by the outdated version
                 // If we don't reconsider the block we will be permanently "stuck"
                 // We can use the checkpoint to find out if this is the case, so we do this here.
                 // If we are the ancestor of a checkpoint then reset the failiure flags and try again
