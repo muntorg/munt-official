0bfcf0d24c366e00a3e41ab581fda060cc3d8c21
Malcolm MacLeod <malcolm@gulden.com>
2019-09-16 15:12:11 +0200

    QA: Build fix

--- src/crypto/hash/sigma/argon_echo/opt/core_opt_arm_cortex_a72_aes.h
+++ src/crypto/hash/sigma/argon_echo/opt/core_opt_arm_cortex_a72_aes.h
@@ -5,9 +5,9 @@
 // This file is a thin wrapper around the actual 'argon2_echo_opt', along with various other similarly named files.
 // The build system compiles each file with slightly different optimisation flags so that we have optimised implementations for a wide spread of processors.
 
-#ifndef HASH_ARGON2_ECHO_ARM_THUNDERX_AES_H
-#define HASH_ARGON2_ECHO_ARM_THUNDERX_AES_H
-    #define argon2_echo_ctx argon2_echo_ctx_arm_thunderx_aes
+#ifndef HASH_ARGON2_ECHO_ARM_CORTEX_A72_AES_H
+#define HASH_ARGON2_ECHO_ARM_CORTEX_A72_AES_H
+    #define argon2_echo_ctx argon2_echo_ctx_arm_cortex_a72_aes
 
     #define ARGON2_CORE_OPT_IMPL
     #include "../argon_echo.h"
--- src/crypto/hash/sigma/argon_echo/opt/core_opt_arm_thunderx_aes.cpp
+++ src/crypto/hash/sigma/argon_echo/opt/core_opt_arm_thunderx_aes.cpp
@@ -6,7 +6,7 @@
 // This file is a thin wrapper around the actual 'argon2_echo_opt' implementation, along with various other similarly named files.
 // The build system compiles each file with slightly different optimisation flags so that we have optimised implementations for a wide spread of processors.
 
-#if defined(COMPILER_HAS_CORTEX72_AES)
+#if defined(COMPILER_HAS_THUNDERX_AES)
 
 #include <stdint.h>
 #include <string.h>
@@ -19,32 +19,32 @@
 
 #define ARGON2_BLOCK_WORD_SIZE __m128i
 #define ARGON2_BLOCK_WORD_COUNT ARGON2_OWORDS_IN_BLOCK
-#define next_addresses     arm_cortex_a72_aes_next_addresses
-#define fill_segment       arm_cortex_a72_aes_fill_segment
-#define fill_block         arm_cortex_a72_aes_fill_block
-#define initialize         arm_cortex_a72_aes_initialize
-#define fill_memory_blocks arm_cortex_a72_aes_fill_memory_blocks
-#define Compress           arm_cortex_a72_aes_argon2_echo_compress
-#define init_block_value   arm_cortex_a72_aes_argon2_echo_init_block_value
-#define copy_block         arm_cortex_a72_aes_argon2_echo_copy_block_value
-#define xor_block          arm_cortex_a72_aes_argon2_echo_xor_block_value
-#define finalize           arm_cortex_a72_aes_argon2_echo_finalize
-#define index_alpha        arm_cortex_a72_aes_argon2_echo_index_alpha
-#define initial_hash       arm_cortex_a72_aes_argon2_echo_initial_hash
-#define fill_first_blocks  arm_cortex_a72_aes_argon2_echo_fill_first_blocks
+#define next_addresses     arm_thunderx_aes_next_addresses
+#define fill_segment       arm_thunderx_aes_fill_segment
+#define fill_block         arm_thunderx_aes_fill_block
+#define initialize         arm_thunderx_aes_initialize
+#define fill_memory_blocks arm_thunderx_aes_fill_memory_blocks
+#define Compress           arm_thunderx_aes_argon2_echo_compress
+#define init_block_value   arm_thunderx_aes_argon2_echo_init_block_value
+#define copy_block         arm_thunderx_aes_argon2_echo_copy_block_value
+#define xor_block          arm_thunderx_aes_argon2_echo_xor_block_value
+#define finalize           arm_thunderx_aes_argon2_echo_finalize
+#define index_alpha        arm_thunderx_aes_argon2_echo_index_alpha
+#define initial_hash       arm_thunderx_aes_argon2_echo_initial_hash
+#define fill_first_blocks  arm_thunderx_aes_argon2_echo_fill_first_blocks
 
-#define argon2_echo_ctx    argon2_echo_ctx_arm_cortex_a72_aes
+#define argon2_echo_ctx    argon2_echo_ctx_arm_thunderx_aes
 
-#define ECHO_HASH_256(DATA, DATABYTELEN, HASH)                                               \
-{                                                                                            \
-    echo256_opt_hashState ctx_echo;                                                          \
-    echo256_opt_arm_cortex_a72_Init(&ctx_echo);                                              \
-    echo256_opt_arm_cortex_a72_Update(&ctx_echo, (const unsigned char*)(DATA), DATABYTELEN); \
-    echo256_opt_arm_cortex_a72_Final(&ctx_echo, HASH);                                       \
+#define ECHO_HASH_256(DATA, DATABYTELEN, HASH)                                             \
+{                                                                                          \
+    echo256_opt_hashState ctx_echo;                                                        \
+    echo256_opt_arm_thunderx_Init(&ctx_echo);                                              \
+    echo256_opt_arm_thunderx_Update(&ctx_echo, (const unsigned char*)(DATA), DATABYTELEN); \
+    echo256_opt_arm_thunderx_Final(&ctx_echo, HASH);                                       \
 }
 
 //fixme: (SIGMA) (CBSU) - This is just a direct copy of the SSE2 version that we compile with substituted neon intrinsics and flags; it might theoretically be possible to speed this up with explicit NEON intrinsic design.
-static void arm_cortex_a72_aes_fill_block(__m128i *state, const argon2_echo_block *ref_block, argon2_echo_block *next_block, int with_xor)
+static void arm_thunderx_aes_fill_block(__m128i *state, const argon2_echo_block *ref_block, argon2_echo_block *next_block, int with_xor)
 {
     __m128i block_XY[ARGON2_OWORDS_IN_BLOCK];
     unsigned int i;
