c54ddb2a8d61b146cc52efff1a0fedf136742e82
Malcolm MacLeod <malcolm@gulden.com>
2019-09-25 21:06:34 +0200

    MISC: Bump testnet SIGMA activation date to 1569510000.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -382,7 +382,7 @@ public:
 
             if (sTestnetParams == "C1534687770:60")
             {
-                defaultSigmaSettings.activationDate = 1569423600;
+                defaultSigmaSettings.activationDate = 1569510000;
             }
             else
             {
