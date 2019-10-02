dcdd59344823a2fd8f16adaf717409bc75a018bd
Willem de Jonge <willem@isnapp.nl>
2019-10-02 14:40:45 +0200

    Fix sigma delta drop adjustment

--- src/Gulden/Common/diff_delta.cpp
+++ src/Gulden/Common/diff_delta.cpp
@@ -306,7 +306,7 @@ unsigned int GetNextWorkRequired_DELTA (const INDEX_TYPE pindexLast, const BLOCK
         int64_t nNumMissedSteps = ((BLOCK_TIME(block) - INDEX_TIME(pindexLast) - nLongTimeLimit) / nLongTimeStep) + 1;
         for(int i=0;i < nNumMissedSteps; ++i)
         {
-            bnNew = BIGINT_MULTIPLY(bnNew, arith_uint256(110));
+            bnNew = BIGINT_MULTIPLY(bnNew, arith_uint256(nDeltaDropPerStep));
             bnNew = BIGINT_DIVIDE(bnNew, arith_uint256(PERCENT_FACTOR));
         }
 
