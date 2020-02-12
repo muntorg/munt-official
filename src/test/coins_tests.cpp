bf5baa3ff5a6d7e6f0862c9ab8c320a341828afc
Malcolm MacLeod <malcolm@gulden.com>
2020-02-12 11:48:08 +0200

    QA: Get ccoins_serialization passing, all 'cons_tests' should now pass.

--- src/test/coins_tests.cpp
+++ src/test/coins_tests.cpp
@@ -554,12 +554,11 @@ BOOST_AUTO_TEST_CASE(updatecoins_simulation_test)
     BOOST_CHECK(spent_a_duplicate_coinbase);
 }
 
-#if 0
+
 BOOST_AUTO_TEST_CASE(ccoins_serialization)
 {
-    //*
     // Good example
-    CDataStream ss1(ParseHex("97f23c005847f80d0000001976a914816115944e077fe7c803cfa57f29b36bf87c1d3588ac"), SER_DISK, CLIENT_VERSION);
+    CDataStream ss1(ParseHex("b0e57800005847f80d0000001976a914816115944e077fe7c803cfa57f29b36bf87c1d3588ac"), SER_DISK, CLIENT_VERSION);
     Coin cc1;
     ss1 >> cc1;
     BOOST_CHECK_EQUAL(cc1.fCoinBase, false);
@@ -568,16 +567,16 @@ BOOST_AUTO_TEST_CASE(ccoins_serialization)
     BOOST_CHECK_EQUAL(HexStr(cc1.out.output.scriptPubKey), HexStr(GetScriptForDestination(CKeyID(uint160(ParseHex("816115944e077fe7c803cfa57f29b36bf87c1d35"))))));
 
     // Good example
-    CDataStream ss2(ParseHex("8ddf773daf0100000000001976a9148c988f1a4a4de2161e0f50aac7f17e7f9555caa488ac"), SER_DISK, CLIENT_VERSION);
+    CDataStream ss2(ParseHex("9cc06d003daf0100000000001976a9148c988f1a4a4de2161e0f50aac7f17e7f9555caa488ac"), SER_DISK, CLIENT_VERSION);
     Coin cc2;
     ss2 >> cc2;
     BOOST_CHECK_EQUAL(cc2.fCoinBase, true);
     BOOST_CHECK_EQUAL(cc2.nHeight, 120891U);
     BOOST_CHECK_EQUAL(cc2.out.nValue, 110397);
-    BOOST_CHECK_EQUAL(HexStr(cc2.out.output.scriptPubKey), HexStr(GetScriptForDestination(CKeyID(uint160(ParseHex("8c988f1a4a4de2161e0f50aac7f17e7f9555caa4"))))));
+    BOOST_CHECK_EQUAL(HexStr(cc2.out.output.scriptPubKey), HexStr(GetScriptForDestination(CKeyID(uint160(ParseHex("8c988f1a4a4de2161e0f50aac7f17e7f9555caa4"))))));    
 
     // Smallest possible example
-    CDataStream ss3(ParseHex("00000000000000000000"), SER_DISK, CLIENT_VERSION);
+    CDataStream ss3(ParseHex("00000000000000000000000"), SER_DISK, CLIENT_VERSION);
     Coin cc3;
     ss3 >> cc3;
     BOOST_CHECK_EQUAL(cc3.fCoinBase, false);
@@ -586,7 +585,7 @@ BOOST_AUTO_TEST_CASE(ccoins_serialization)
     BOOST_CHECK_EQUAL(cc3.out.output.scriptPubKey.size(), 0U);
 
     // scriptPubKey that ends beyond the end of the stream
-    CDataStream ss4(ParseHex("000007"), SER_DISK, CLIENT_VERSION);
+    CDataStream ss4(ParseHex("00000007"), SER_DISK, CLIENT_VERSION);
     try {
         Coin cc4;
         ss4 >> cc4;
@@ -607,7 +606,7 @@ BOOST_AUTO_TEST_CASE(ccoins_serialization)
     } catch (const std::ios_base::failure& e) {
     }
 }
-#endif
+
 
 const static COutPoint OUTPOINT;
 const static CAmount PRUNED = -1;
