d5cce03c643a25020e5b500263520c72106f8844
Malcolm MacLeod <malcolm@gulden.com>
2020-03-08 21:00:10 +0200

    CORE: Bump protocol version for phase4 release

--- src/version.h
+++ src/version.h
@@ -16,8 +16,7 @@
  * network protocol versioning
  */
 
-//fixme: (PHASE4) - Bump proto version for phase 4 - see MIN_PEER_PROTO_VERSION phase5 fixme for more information
-static const int PROTOCOL_VERSION = 70016;
+static const int PROTOCOL_VERSION = 70017;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -28,7 +27,7 @@ static const int GETHEADERS_VERSION = 31800;
 //fixme: (PHASE5) - After phase4 activation bump min proto version
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 70014;
-#define MIN_PEER_PROTO_VERSION (IsPow2Phase4Active(chainActive.Tip(), chainparams, chainActive) ? MIN_PEER_PROTO_VERSION + 1 : MIN_PEER_PROTO_VERSION)
+#define MIN_PEER_PROTO_VERSION (IsPow2Phase4Active(chainActive.Tip(), chainparams, chainActive) ? PROTOCOL_VERSION : MIN_PEER_PROTO_VERSION)
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
