b82d412f0310a3d08f1d16ad00e8759c8d73db59
Malcolm MacLeod <malcolm@gulden.com>
2020-03-20 16:18:54 +0200

    QA: Minor fixme triage

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -110,7 +110,7 @@ struct PrecomputedTransactionData
     PrecomputedTransactionData(const CTransaction& tx);
 };
 
-//fixme: (PHASE4) SEGSIG
+//fixme: (PHASE4) (RELEASE) - go through all occurences of this one more time and check usages all make sense
 enum SigVersion
 {
     SIGVERSION_BASE = 0,
