0e1e92c401e3ce79cd608e0a977a824c0d6b30e7
Malcolm MacLeod <malcolm@gulden.com>
2020-04-04 22:40:48 +0200

    CORE: Bump min protocol version post phase4 activation
    We no longer need the expensive check for this, we are now activated

--- src/version.h
+++ src/version.h
@@ -24,10 +24,8 @@ static const int INIT_PROTO_VERSION = 209;
 //! In this version, 'getheaders' was introduced.
 static const int GETHEADERS_VERSION = 31800;
 
-//fixme: (PHASE5) - After phase4 activation bump min proto version
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70014;
-#define MIN_PEER_PROTO_VERSION (IsPow2Phase4Active(chainActive.Tip()) ? PROTOCOL_VERSION : MIN_PEER_PROTO_VERSION)
+static const int MIN_PEER_PROTO_VERSION = 70017;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
