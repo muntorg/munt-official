fb815eb2dacbe57a628665c82e5c9709ff89f3d9
Malcolm MacLeod <malcolm@gulden.com>
2020-05-07 14:07:13 +0200

    ELECTRON: BUILD: Update build script for mingw64/msys2

--- developer-tools/build_electron.sh
+++ developer-tools/build_electron.sh
@@ -17,7 +17,7 @@ if [ ${PLATFORM_VENDOR} = "apple" ]; then
     export LDFLAGS="-fPIC -Bsymbolic"
 else
     if [ ${PLATFORM_OS} = "mingw64" ]; then
-        export LDFLAGS="-fPIC -Bsymbolic -L${DIR}/../build_electron -lnode -Wl,--gc-sections"
+        export LDFLAGS="-fPIC -Bsymbolic -L${DIR}/../build_electron -lelectron -Wl,--gc-sections"
         export EXTRA_CXX_FLAGS="-DNODE_GYP_MODULE_NAME=guldenunifiedbackend -DUSING_UV_SHARED=1 -DUSING_V8_SHARED=1 -DV8_DEPRECATION_WARNINGS=1 -DV8_DEPRECATION_WARNINGS -DV8_IMMINENT_DEPRECATION_WARNINGS -DWIN32 -D_CRT_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_DEPRECATE -D_HAS_EXCEPTIONS=0 -DBUILDING_NODE_EXTENSION -D_WINDLL"
     else
         export LDFLAGS="-fPIC -Bsymbolic -Wl,--gc-sections"
@@ -40,11 +40,11 @@ tar -xvf iojs-v${ELECTRON_VERSION}.tar.gz -C electron-${ELECTRON_VERSION}
 mkdir node-addon-api-${NAPI_VERSION} | true
 tar -xvf node-addon-api-${NAPI_VERSION}.tgz -C node-addon-api-${NAPI_VERSION}
 
-export CXXFLAGS="${CXXFLAGS} -I${DIR}/../build_electron/electron-${ELECTRON_VERSION}/node_headers/include/node/ -I${DIR}/../build_electron/node-addon-api-${NAPI_VERSION}/package/ $(EXTRA_CXX_FLAGS} -O2"
+export CXXFLAGS="${CXXFLAGS} -I${DIR}/../build_electron/electron-${ELECTRON_VERSION}/node_headers/include/node/ -I${DIR}/../build_electron/node-addon-api-${NAPI_VERSION}/package/ ${EXTRA_CXX_FLAGS} -O2"
 export CFLAGS=${CXXFLAGS}
 
 if [ ${PLATFORM_OS} = "mingw64" ]; then
-    dlltool -d ${DIR}/electron/electron.def -l libnode.a
+    dlltool -d ${DIR}/electron/electron.def -l libelectron.a
 fi
 
 ../autogen.sh
