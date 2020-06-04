328d6c8dedbb08f7ee938517ca112b54c73a4965
Malcolm MacLeod <malcolm@gulden.com>
2020-06-04 14:23:18 +0200

    CORE: Get rid of unnecessary BIP34Hash from chainparams

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -63,7 +63,6 @@ struct Params {
     uint256 hashGenesisBlock;
     /** Block height and hash at which BIP34 becomes active */
     int BIP34Height;
-    uint256 BIP34Hash;
     /** Block height at which BIP65 becomes active */
     int BIP65Height;
     /** Block height at which BIP66 becomes active */
