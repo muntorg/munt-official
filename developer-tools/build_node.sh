39c050a14af8fb3c89f95478d431a2bc4d3a8978
Malcolm MacLeod <malcolm@gulden.com>
2020-06-08 20:12:46 +0200

    BUILD: Minor build improvements

--- developer-tools/build_node.sh
+++ developer-tools/build_node.sh
@@ -21,7 +21,7 @@ if [ ${PLATFORM_VENDOR} = "apple" ]; then
     export LDFLAGS="-fPIC -Bsymbolic -Wl,-undefined -Wl,dynamic_lookup"
 else
     if [ ${PLATFORM_OS} = "mingw32" ] ||  [ ${PLATFORM_OS} = "mingw64" ]; then
-        export LDFLAGS="-fPIC -Bsymbolic -L${DIR}/../build_electron -lnode -Wl,--gc-sections"
+        export LDFLAGS="-fPIC -Bsymbolic -L${DIR}/../build_node -lnode -Wl,--gc-sections"
         export EXTRA_CXX_FLAGS="-DNODE_HOST_BINARY=node.exe -DUSING_UV_SHARED=1 -DUSING_V8_SHARED=1 -DV8_DEPRECATION_WARNINGS=1 -DV8_DEPRECATION_WARNINGS -DV8_IMMINENT_DEPRECATION_WARNINGS -DWIN32 -D_CRT_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_DEPRECATE -D_HAS_EXCEPTIONS=0 -DBUILDING_NODE_EXTENSION -D_WINDLL"
     else
         export LDFLAGS="-fPIC -Bsymbolic -Wl,--gc-sections"
@@ -33,8 +33,8 @@ cd depends
 make NO_QT=1 NO_UPNP=1 EXTRA_PACKAGES='qrencode' -j ${NUM_PROCS}
 cd ..
 
-mkdir build_electron | true
-cd build_electron
+mkdir build_node | true
+cd build_node
 
 wget -c https://atom.io/download/electron/v${ELECTRON_VERSION}/iojs-v${ELECTRON_VERSION}.tar.gz
 wget -c https://registry.npmjs.org/node-addon-api/-/node-addon-api-${NAPI_VERSION}.tgz
@@ -44,7 +44,7 @@ tar -xvf iojs-v${ELECTRON_VERSION}.tar.gz -C electron-${ELECTRON_VERSION}
 mkdir node-addon-api-${NAPI_VERSION} | true
 tar -xvf node-addon-api-${NAPI_VERSION}.tgz -C node-addon-api-${NAPI_VERSION}
 
-export CXXFLAGS="${CXXFLAGS} -I${DIR}/../build_electron/electron-${ELECTRON_VERSION}/node_headers/include/node/ -I${DIR}/../build_electron/node-addon-api-${NAPI_VERSION}/package/ ${EXTRA_CXX_FLAGS} -O2"
+export CXXFLAGS="${CXXFLAGS} -I${DIR}/../build_node/electron-${ELECTRON_VERSION}/node_headers/include/node/ -I${DIR}/../build_node/node-addon-api-${NAPI_VERSION}/package/ ${EXTRA_CXX_FLAGS} -O2"
 export CFLAGS=${CXXFLAGS}
 
 if [ ${PLATFORM_OS} = "mingw32" ] || [ ${PLATFORM_OS} = "mingw64" ]; then
@@ -56,4 +56,12 @@ fi
 CONFIG_SITE='' ../configure --prefix=$PWD/../depends/${PLATFORM} --disable-bench --disable-tests --disable-man --disable-zmq --without-utils  --without-daemon --with-node-js-libs --with-qrencode --with-gui=no
 make V=1 -j ${NUM_PROCS}
 cd ..
+
+if test -f "build_node/src/.libs/libgulden_unity_node_js-0.dll"; then
+    cp build_node/src/.libs/libgulden_unity_node_js-0.dll src/frontend/electron/libnovo_unity_node_js.node
+    cp build_node/src/.libs/libgulden_unity_node_js-0.dll src/frontend/electron_sample/libnovo_unity_node_js.node
+elif test -f "build_node/src/.libs/libgulden_unity_node_js.so"; then
+    cp build_node/src/.libs/libgulden_unity_node_js.so src/frontend/electron/libnovo_unity_node_js.node
+    cp build_node/src/.libs/libgulden_unity_node_js.so src/frontend/electron_sample/libnovo_unity_node_js.node
+fi
 
