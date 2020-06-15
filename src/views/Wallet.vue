7943bbebbd82f39335f3022e3c3fad5f1eb9c1f2
Rijk Plasman <rijkplasman@users.noreply.github.com>
2020-06-15 07:40:13 +0200

    removed text select, added transfer button

--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -4,6 +4,7 @@
       <h4>{{ $t("balance.your_address") }}</h4>
       <span id="address">{{ receiveAddress }}</span>
     </div>
+    <div class="transfer"><a target="_blank" href="https://novocurrency.com/transfer?receive_address={{ receiveAddress }}" id="transfer">Receive your acquired Novo</a></div>
   </div>
 </template>
 
