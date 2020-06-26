e328f54ff05c80e4366b58bd5690d85f769ac70d
Vincent van der Kroef <devlz303@gmail.com>
2020-06-26 20:55:16 +0200

    move loader html/css to AppLoader component

--- src/frontend/electron_vue/src/App.vue
+++ src/frontend/electron_vue/src/App.vue
@@ -1,27 +1,13 @@
 <template>
   <div id="app">
-    <!-- app loader -->
-    <div id="app-loader" v-show="showLoader">
-      <div class="logo-outer">
-        <div class="logo-inner"></div>
-      </div>
-      <div class="version-container">
-        <span>Unity: {{ unityVersion }}</span>
-        <span class="divider">|</span>
-        <span>Wallet: {{ walletVersion }}</span>
-        <span class="divider">|</span>
-        <span>Electron: {{ electronVersion }}</span>
-      </div>
-      <div class="info">
-        <p v-show="isShuttingDown">
-          {{ $t("loader.shutdown") }}
-        </p>
-        <div v-show="isSynchronizing">
-          <!-- todo: add progress bar -->
-          <div>{{ $t("loader.synchronizing") }}</div>
-        </div>
-      </div>
-    </div>
+    <app-loader
+      v-show="showLoader"
+      :unityVersion="unityVersion"
+      :walletVersion="walletVersion"
+      :electronVersion="electronVersion"
+      :isShuttingDown="isShuttingDown"
+      :isSynchronizing="isSynchronizing"
+    />
 
     <!-- main application -->
     <div>
@@ -50,6 +36,7 @@
 <script>
 import { mapState } from "vuex";
 import { AppStatus } from "./store";
+import AppLoader from "./components/AppLoader";
 
 export default {
   data() {
@@ -58,6 +45,9 @@ export default {
       electronVersion: process.versions.electron,
       progress: 1
     };
+  },
+  components: {
+    AppLoader
   },
   watch: {
     status() {
@@ -145,32 +135,6 @@ export default {
   background-color: #fff;
 }
 
-#app-loader {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  margin-top: 0;
-  margin-left: 0;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: center;
-  background-color: #fff;
-  z-index: 1;
-}
-
-#app-loader .version-container {
-  margin: 16px 0;
-}
-
-#app-loader .version-container .divider {
-  margin: 0 8px;
-}
-
-#app-loader .info {
-  min-height: 100px;
-}
-
 .app-top {
   position: absolute;
   top: 0;
@@ -222,26 +186,6 @@ export default {
   width: 22px;
   height: 22px;
   background: url("./img/logo.svg"), linear-gradient(transparent, transparent);
-}
-
-.logo-outer {
-  margin: 24px;
-  background: #009572;
-  width: 128px;
-  height: 128px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo-inner {
-  width: 64px;
-  height: 64px;
-  background: url("./img/logo.svg");
-  background-size: cover;
 }
 
 .app-balance {
---
+++ src/frontend/electron_vue/src/components/AppLoader.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="app-loader">
+    <div class="logo-outer">
+      <div class="logo-inner"></div>
+    </div>
+    <div class="version-container">
+      <span>Unity: {{ unityVersion }}</span>
+      <span class="divider">|</span>
+      <span>Wallet: {{ walletVersion }}</span>
+      <span class="divider">|</span>
+      <span>Electron: {{ electronVersion }}</span>
+    </div>
+    <div class="info">
+      <p v-show="isShuttingDown">
+        {{ $t("loader.shutdown") }}
+      </p>
+      <div v-show="isSynchronizing">
+        <!-- todo: add progress bar -->
+        <div>{{ $t("loader.synchronizing") }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppLoader",
+  props: {
+    unityVersion: {
+      type: String,
+      default: "0.0.0"
+    },
+    walletVersion: {
+      type: String,
+      default: "0.0.0"
+    },
+    electronVersion: {
+      type: String,
+      default: "0.0.0"
+    },
+    isShuttingDown: {
+      type: Boolean,
+      default: false
+    },
+    isSynchronizing: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.app-loader {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin-top: 0;
+  margin-left: 0;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #fff;
+  z-index: 9999;
+}
+
+.logo-outer {
+  margin: 24px;
+  background: #009572;
+  width: 128px;
+  height: 128px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+
+.logo-inner {
+  width: 64px;
+  height: 64px;
+  background: url("../img/logo.svg");
+  background-size: cover;
+}
+
+.version-container {
+  margin: 16px 0;
+
+  & .divider {
+    margin: 0 8px;
+  }
+}
+
+.info {
+  min-height: 100px;
+}
+</style>
