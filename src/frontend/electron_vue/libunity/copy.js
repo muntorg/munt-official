9c109a54376b666c22650431cc956e2fdd7fe17a
Vincent van der Kroef <devlz303@gmail.com>
2020-06-26 16:18:58 +0200

    show error when download of .node file fails

--- src/frontend/electron_vue/libunity/copy.js
+++ src/frontend/electron_vue/libunity/copy.js
@@ -1,34 +1,38 @@
-var os = require('os');
+var os = require("os");
 
-let repo = "https://github.com/novocurrency/novocurrency-core/releases/latest/download/"
-let file = ""
-if (os.platform() === "win32")
-{
-  file = "libnovo_win_"+os.arch()+".node"
-}
-else if(os.platform() === "linux")
-{
-  file = "libnovo_linux_"+os.arch()+".node"
-}
-else if (os.platform() === "darwin")
-{
-  file = "libnovo_macos_"+os.arch()+".node"
-}
-else
-{
-    throw "Unable to determine platform";
+let repo =
+  "https://github.com/novocurrency/novocurrency-core/releases/latest/download/";
+
+let file = "";
+if (os.platform() === "win32") {
+  file = "libnovo_win_" + os.arch() + ".node";
+} else if (os.platform() === "linux") {
+  file = "libnovo_linux_" + os.arch() + ".node";
+} else if (os.platform() === "darwin") {
+  file = "libnovo_macos_" + os.arch() + ".node";
+} else {
+  throw "Unable to determine platform";
 }
 
-const fs = require('fs');
-const request = require('request');
+const fs = require("fs");
+const request = require("request");
 const download = (uri, filename, callback) => {
-    request.head(uri, (err, res, body) => {
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
+  request.head(uri, (err, res) => {
+    if (err) {
+      return console.error(err);
+    }
+    if (res.statusCode !== 200) {
+      return console.error(`Error downloading ${filename}: ${res.statusCode}`);
+    }
+
+    request(uri)
+      .pipe(fs.createWriteStream(filename))
+      .on("close", callback);
+  });
 };
 
 const path = require("path");
-let src = repo+file;
+let src = repo + file;
 let dst = path.join(__dirname, `../src/unity/lib_unity.node`);
 download(src, dst, () => {
   console.log(`fetched ${src} -> ${dst}`);
