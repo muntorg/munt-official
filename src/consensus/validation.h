74d28ebaaad26881aca39a193f700083c634b9fe
Malcolm MacLeod <malcolm@gulden.com>
2020-08-01 14:35:15 +0200

    CORE: Bump minimum witness period up to two months

--- src/consensus/validation.h
+++ src/consensus/validation.h
@@ -27,8 +27,8 @@ static const int gMaximumParticipationAge = 365 * gRefactorDailyBlocksUsage; // 
                                                     // We try to balance this in such a way that it allows smaller witness accounts but not ones so absurdly small that they force witnesses to unnecessarily keep years of data around.
                                                     // Currently set at - 1 year (365 days * daily blocks).
 
-static const int gStartingWitnessNetworkWeightEstimate  = 260000000;
-static const int gMinimumWitnessLockDays                = 30;
+static const int gStartingWitnessNetworkWeightEstimate  = 5000;
+static const int gMinimumWitnessLockDays                = 60;
 static const int gMaximumWitnessLockDays                = 3 * 365;
 static const int gMaximumWitnessCompoundAmount = 40;
 
