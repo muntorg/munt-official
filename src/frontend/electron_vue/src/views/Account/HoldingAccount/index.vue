37b577eee0251a6c1ce97b43d8088471cd3ab5fd
Vincent van der Kroef <devlz303@gmail.com>
2020-08-03 16:24:32 +0200

    improve holding statistics UI

--- src/frontend/electron_vue/src/views/Account/HoldingAccount/index.vue
+++ src/frontend/electron_vue/src/views/Account/HoldingAccount/index.vue
@@ -15,7 +15,50 @@
       </section>
     </portal>
 
-    <pre>{{ statistics }}</pre>
+    <novo-section class="holding-information">
+      <h4>information</h4>
+
+      <div class="flex-row">
+        <div>Status</div>
+        <div>{{ accountStatus }}</div>
+      </div>
+      <div class="flex-row">
+        <div>Locked from block</div>
+        <div>{{ lockedFrom }}</div>
+      </div>
+      <div class="flex-row">
+        <div>Locked until block</div>
+        <div>
+          {{ lockedUntil }}
+        </div>
+      </div>
+      <div class="flex-row">
+        <div>Lock duration</div>
+        <div>{{ lockDuration }} blocks</div>
+      </div>
+      <div class="flex-row">
+        <div>Remaining lock period</div>
+        <div>{{ remainingLockPeriod }} blocks</div>
+      </div>
+
+      <div class="flex-row">
+        <div>Required earnings frequency</div>
+        <div>{{ requiredEarningsFrequency }} blocks</div>
+      </div>
+      <div class="flex-row">
+        <div>Expected earnings frequency</div>
+        <div>{{ expectedEarningsFrequency }} blocks</div>
+      </div>
+
+      <div class="flex-row">
+        <div>Account weight</div>
+        <div>{{ accountWeight }}</div>
+      </div>
+      <div class="flex-row">
+        <div>Network weight</div>
+        <div>{{ networkWeight }}</div>
+      </div>
+    </novo-section>
 
     <portal to="footer-slot">
       <div />
@@ -39,6 +82,38 @@ export default {
       rightSectionComponent: null,
       statistics: null
     };
+  },
+  computed: {
+    accountStatus() {
+      return this.statistics.account_status || null;
+    },
+    lockedFrom() {
+      return this.statistics.account_initial_lock_creation_block_height || null;
+    },
+    lockedUntil() {
+      return (
+        this.statistics.account_initial_lock_creation_block_height +
+          this.statistics.account_initial_lock_period_in_blocks || null
+      );
+    },
+    lockDuration() {
+      return this.statistics.account_initial_lock_period_in_blocks || null;
+    },
+    remainingLockPeriod() {
+      return this.statistics.account_remaining_lock_period_in_blocks || null;
+    },
+    requiredEarningsFrequency() {
+      return this.statistics.account_expected_witness_period_in_blocks || null;
+    },
+    expectedEarningsFrequency() {
+      return this.statistics.account_estimated_witness_period_in_blocks || null;
+    },
+    accountWeight() {
+      return this.statistics.account_weight || null;
+    },
+    networkWeight() {
+      return this.statistics.network_tip_total_weight || null;
+    }
   },
   created() {
     this.updateStatistics();
@@ -87,5 +162,12 @@ export default {
       }
     }
   }
+}
+
+.flex-row > div {
+  line-height: 18px;
+}
+.flex-row :first-child {
+  min-width: 220px;
 }
 </style>
