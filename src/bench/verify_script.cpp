a4a1f82ffc34e81c078c0f820b6ba7696233db5d
Malcolm MacLeod <malcolm@gulden.com>
2020-08-31 11:52:41 +0200

    CORE: Get bench_sigma tool working again

--- src/bench/verify_script.cpp
+++ src/bench/verify_script.cpp
@@ -14,7 +14,7 @@
 // FIXME: (Bitcoin) Dedup with BuildCreditingTransaction in test/script_tests.cpp.
 static CMutableTransaction BuildCreditingTransaction(const CScript& scriptPubKey)
 {
-    CMutableTransaction txCredit;
+    CMutableTransaction txCredit(1);
     txCredit.nVersion = 1;
     txCredit.nLockTime = 0;
     txCredit.vin.resize(1);
@@ -22,8 +22,8 @@ static CMutableTransaction BuildCreditingTransaction(const CScript& scriptPubKey
     txCredit.vin[0].prevout.SetNull();
     txCredit.vin[0].scriptSig = CScript() << CScriptNum(0) << CScriptNum(0);
     //fixme: (PHASE5)
-    txCredit.vin[0].nSequence = CTxIn::SEQUENCE_FINAL;
-    txCredit.vout[0].scriptPubKey = scriptPubKey;
+    txCredit.vin[0].SetSequence(CTxIn::SEQUENCE_FINAL, 1, CTxInFlags::None);
+    txCredit.vout[0].output.scriptPubKey = scriptPubKey;
     txCredit.vout[0].nValue = 1;
 
     return txCredit;
@@ -32,17 +32,17 @@ static CMutableTransaction BuildCreditingTransaction(const CScript& scriptPubKey
 // FIXME: Dedup with BuildSpendingTransaction in test/script_tests.cpp.
 static CMutableTransaction BuildSpendingTransaction(const CScript& scriptSig, const CMutableTransaction& txCredit)
 {
-    CMutableTransaction txSpend;
+    CMutableTransaction txSpend(1);
     txSpend.nVersion = 1;
     txSpend.nLockTime = 0;
     txSpend.vin.resize(1);
     txSpend.vout.resize(1);
-    txSpend.vin[0].prevout.hash = txCredit.GetHash();
+    txSpend.vin[0].prevout.setHash(txCredit.GetHash());
     txSpend.vin[0].prevout.n = 0;
     txSpend.vin[0].scriptSig = scriptSig;
     //fixme: (PHASE5)
-    txSpend.vin[0].nSequence = CTxIn::SEQUENCE_FINAL;
-    txSpend.vout[0].scriptPubKey = CScript();
+    txSpend.vin[0].SetSequence(CTxIn::SEQUENCE_FINAL, 1, CTxInFlags::None);
+    txSpend.vout[0].output.scriptPubKey = CScript();
     txSpend.vout[0].nValue = txCredit.vout[0].nValue;
 
     return txSpend;
