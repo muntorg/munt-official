e5cb1d847607fd88d48543a7d02a6a1e4df8638a
Malcolm MacLeod <malcolm@gulden.com>
2020-11-11 14:42:05 +0200

    CORE: Simplify DecodeHexBlk now that segsig is locked in

--- src/core_read.cpp
+++ src/core_read.cpp
@@ -160,33 +160,15 @@ bool DecodeHexBlk(CBlock& block, const std::string& strHexBlk)
     if (!IsHex(strHexBlk))
         return false;
 
+    std::vector<unsigned char> blockData(ParseHex(strHexBlk));
+    CDataStream ssBlock(blockData, SER_NETWORK, PROTOCOL_VERSION);
+    try
     {
-        bool parsedAsSegSig = true;
-        std::vector<unsigned char> blockData(ParseHex(strHexBlk));
-        CDataStream ssBlock(blockData, SER_NETWORK, PROTOCOL_VERSION | SERIALIZE_BLOCK_HEADER_NO_POW2_WITNESS);
-        try
-        {
-            ssBlock >> block;
-        }
-        catch (...)
-        {
-            parsedAsSegSig = false;
-        }
-        if (parsedAsSegSig)
-            return true;
+        ssBlock >> block;
     }
-    //fixme: (PHASE5) We can remove this code block after phase 4 activates.
+    catch (const std::exception& e)
     {
-        std::vector<unsigned char> blockData(ParseHex(strHexBlk));
-        CDataStream ssBlock(blockData, SER_NETWORK, PROTOCOL_VERSION);
-        try
-        {
-            ssBlock >> block;
-        }
-        catch (const std::exception& e)
-        {
-            return false;
-        }
+        return false;
     }
 
     return true;
