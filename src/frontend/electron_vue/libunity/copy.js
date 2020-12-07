0f5346e4ba54d130a7a38a9b58d430ab4739ac57
Malcolm MacLeod <malcolm@gulden.com>
2020-12-07 17:49:51 +0200

    BUILD: Set  up gitian build scripts for electron libs

--- src/frontend/electron_vue/libunity/copy.js
+++ src/frontend/electron_vue/libunity/copy.js
@@ -5,11 +5,11 @@ let repo =
 
 let file = "";
 if (os.platform() === "win32") {
-  file = "libnovo_win_" + os.arch() + ".node";
+  file = "libgulden_win_" + os.arch() + ".node";
 } else if (os.platform() === "linux") {
-  file = "libnovo_linux_" + os.arch() + ".node";
+  file = "libgulden_linux_" + os.arch() + ".node";
 } else if (os.platform() === "darwin") {
-  file = "libnovo_macos_" + os.arch() + ".node";
+  file = "libgulden_macos_" + os.arch() + ".node";
 } else {
   throw "Unable to determine platform";
 }
