971a8ac36e75df4a3d1becdfe09d52eab6d1ebdc
Vincent van der Kroef <devlz303@gmail.com>
2020-12-14 15:43:37 +0100

    add less variables for account background colors

--- src/frontend/electron_vue/src/layouts/AccountsSection.vue
+++ src/frontend/electron_vue/src/layouts/AccountsSection.vue
@@ -225,7 +225,7 @@ export default {
   line-height: 16px;
 
   &:hover {
-    background-color: #292c57;
+    background-color: var(--account-background-color--hover);
   }
 
   & a {
@@ -235,7 +235,7 @@ export default {
 
   &.active {
     color: #fff;
-    background-color: var(--primary-color);
+    background-color: var(--account-background-color--active);
 
     & a {
       color: #fff;
