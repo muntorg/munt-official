a071f41bbb7aba9f2be6fa73b1c979ae9638baeb
Malcolm MacLeod <malcolm@gulden.com>
2021-03-04 22:57:23 +0200

    Revert "minor UI change"
    This reverts commit 9a433514bb992c69fb84e928ea66da27afacb13f.

--- src/frontend/lite/src/views/Account/SpendingAccount/Send.vue
+++ src/frontend/lite/src/views/Account/SpendingAccount/Send.vue
@@ -39,8 +39,8 @@
         {{ $t("buttons.send") }}
       </button>
       <button @click="sellGulden" class="sell-gulden" :disabled="sellDisabled">
-        {{ $t("buttons.sell_gulden") }}
-      </button>
+          {{ $t("buttons.sell_gulden") }}
+        </button>
     </div>
   </div>
 </template>
@@ -195,12 +195,8 @@ input[type="number"]::-webkit-inner-spin-button {
   }
 
   & > .send {
-    margin: 0 30px 0 30px;
-    width: calc(100% - 170px - 30px - 30px - 170px);
-  }
-
-  & > .sell-gulden {
-    width: 170px;
+    margin: 0 0 0 30px;
+    width: calc(100% - 200px);
   }
 }
 </style>
