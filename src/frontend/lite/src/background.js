193e3c9b465ca8450e7bd95a4954c1f5fb4ec170
Malcolm MacLeod <malcolm@gulden.com>
2021-03-12 15:40:16 +0200

    LITE: Temporarily remove gulden URI handler for lite wallets
    Keep guldenlite handler

--- src/frontend/lite/src/background.js
+++ src/frontend/lite/src/background.js
@@ -45,15 +45,11 @@ if (!gotTheLock) {
 }
 if (process.defaultApp) {
   if (process.argv.length >= 2) {
-    app.setAsDefaultProtocolClient("gulden", process.execPath, [
-      path.resolve(process.argv[1])
-    ]);
     app.setAsDefaultProtocolClient("guldenlite", process.execPath, [
       path.resolve(process.argv[1])
     ]);
   }
 } else {
-  app.setAsDefaultProtocolClient("gulden");
   app.setAsDefaultProtocolClient("guldenlite");
 }
 // End of URI handling
