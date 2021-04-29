6bdb1628cee27628abf7fde6414919cab8a40dba
timkoers <timkoers@users.noreply.github.com>
2021-04-29 21:25:41 +0200

    Update bdb.mk
    Didn't meant to remove .old2

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -17,7 +17,7 @@ endef
 
 define $(package)_preprocess_cmds
   chmod a+w dbinc/atomic.h dbinc/atomic.h && \
-  sed -i 's/__atomic_compare_exchange/__atomic_compare_exchange_db/g' dbinc/atomic.h dbinc/atomic.h && \
+  sed -i.old2 's/__atomic_compare_exchange/__atomic_compare_exchange_db/g' dbinc/atomic.h dbinc/atomic.h && \
   sed -i.old 's/atomic_init/atomic_init_db/' dbinc/atomic.h mp/mp_region.c mp/mp_mvcc.c mp/mp_fget.c mutex/mut_method.c mutex/mut_tas.c && \
   cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist
 endef
