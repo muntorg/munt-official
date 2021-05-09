c156b424abbc3fafeb366fefd161db6c472ea861
Malcolm MacLeod <malcolm@gulden.com>
2021-05-09 15:05:07 +0200

    CORE: Fix serialisation issue with witness sync

--- src/chain.h
+++ src/chain.h
@@ -571,9 +571,15 @@ public:
                 READWRITENOSIZEVECTOR(witnessHeaderPoW2Sig);
             }
             
-            if (_nVersion >= 2030013)
+            if( ((s.GetType() == SER_DISK) && (_nVersion>= 2030013)) || 
+                ((s.GetType() == SER_NETWORK) && (_nVersion % 80000 >= WITNESS_SYNC_VERSION)) ||
+                ((s.GetType() == SER_GETHASH) && (witnessUTXODelta.size() > 0)) )
             {
-                READWRITECOMPACTSIZEVECTOR(witnessUTXODelta);
+                if (nVersionPoW2Witness != 0)
+                {
+                    //fixme: (WITNESS_SYNC) - If size is frequently above 200 then switch to varint instead
+                    READWRITECOMPACTSIZEVECTOR(witnessUTXODelta);
+                }
             }
         }
         catch (...)
