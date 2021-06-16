7a0a044d77d626945e8677263826f2b1571d7294
Malcolm MacLeod <malcolm@gulden.com>
2021-06-16 07:50:31 +0200

    BUILD: Move static data into data folder and out of source files

--- developer-tools/gencheckpoints.sh
+++ developer-tools/gencheckpoints.sh
@@ -7,4 +7,4 @@ set -e
 
 #Fetch our private configuration variables and then hand the rest of the work off to python script
 source developer-tools/private.conf
-python developer-tools/python/gencheckpoints.py > src/chainparams_mainnet_static_checkpoint_data.cpp
+python developer-tools/python/gencheckpoints.py > src/data/chainparams_mainnet_static_checkpoint_data.cpp
