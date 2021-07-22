65644dcfaf3f17d43b846be176ef2e7da50d60fc
Malcolm MacLeod <malcolm@gulden.com>
2021-07-22 08:44:43 +0200

    QA: Minor comment change

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1631,9 +1631,10 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
             return false;
         }
 
+
+        // disconnect from peers older than this proto version
         if (nVersion < MIN_PEER_PROTO_VERSION)
         {
-            // disconnect from peers older than this proto version
             if (!gbMinimalLogging)
                 LogPrintf("peer=%d using obsolete version %i; disconnecting\n", pfrom->GetId(), nVersion);
             connman.PushMessage(pfrom, CNetMsgMaker(INIT_PROTO_VERSION).Make(NetMsgType::REJECT, strCommand, REJECT_OBSOLETE, strprintf("Version must be %d or greater", MIN_PEER_PROTO_VERSION)));
