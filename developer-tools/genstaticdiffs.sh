1b9c269672b40af292f9f1a25cf6728bf025fb0b
Malcolm MacLeod <malcolm@gulden.com>
2021-07-27 09:12:19 +0200

    BUILD: Correct genstaticdiffs script
    Regex was changing two values instead of one, make the regex more specific

--- developer-tools/genstaticdiffs.sh
+++ developer-tools/genstaticdiffs.sh
@@ -5,5 +5,5 @@ set -e
 source developer-tools/private.conf
 HEIGHT=$(python developer-tools/python/genstaticdiffs.py)
 sed -i 's/const int32_t nMaxHeight =.*/const int32_t nMaxHeight = '${HEIGHT}';/g' src/pow/diff_old.cpp
-sed -ri 's/DIFF_SWITCHOVER[(]([^,]+).*/DIFF_SWITCHOVER(\1, '${HEIGHT}');/g' src/pow/diff_common.cpp
+sed -ri 's/nOldDiffSwitchoverBlock =([^,]+).*/nOldDiffSwitchoverBlock =\1, '${HEIGHT}');/g' src/pow/diff_common.cpp
 sed -i 's/ //g' ${STATICDIFF_FILENAME}
