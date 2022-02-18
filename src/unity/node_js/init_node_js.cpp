ce5fe0ed4eca38b6c650a9a6680b69eed17be31b
Malcolm MacLeod <malcolm@gulden.com>
2022-02-18 18:00:12 +0200

    CORE: Enable RPC server functinality for node/electron builds

--- src/unity/node_js/init_node_js.cpp
+++ src/unity/node_js/init_node_js.cpp
@@ -10,12 +10,20 @@
 #ifdef ENABLE_WALLET
 #include "wallet/rpcwallet.h"
 #endif
+#include "warnings.h"
 
 #include "unity/djinni/cpp/legacy_wallet_result.hpp"
 #include "unity/djinni/cpp/i_library_controller.hpp"
 
 #include <boost/thread.hpp>
 #include <thread>
+#include <torcontrol.h>
+#include <rpc/server.h>
+#include <httpserver.h>
+#include <httprpc.h>
+#include "ui_interface.h"
+#include "rpc/blockchain.h"
+#include "validation/validation.h"
 
 
 class NodeRPCTimer : public RPCTimerBase
@@ -71,48 +79,95 @@ void InitRegisterRPC()
 
 void ServerInterrupt(boost::thread_group& threadGroup)
 {
-    //InterruptRPC();
+    InterruptHTTPServer();
+    InterruptHTTPRPC();
+    InterruptRPC();
+    InterruptREST();
+    InterruptTorControl();
 }
-
-bool InitRPCWarmup(boost::thread_group& threadGroup)
-{
-    return true;
-}
-
-/*void SetRPCWarmupFinished()
-{
-}*/
-
-/*void RPCNotifyBlockChange(bool ibd, const CBlockIndex * pindex)
-{
-}*/
 
 void ServerShutdown(boost::thread_group& threadGroup)
 {
     RPCUnsetTimerInterface(timerInterface);
-    //StopRPC();
+    StopHTTPServer();
+    StopHTTPRPC();
+    MilliSleep(20); //Allow other threads (UI etc. a chance to cleanup as well)
+    StopRPC();
+    StopREST();
+    MilliSleep(20); //Allow other threads (UI etc. a chance to cleanup as well)
+    StopTorControl();
 }
 
-/*void InitRPCMining()
+static void OnRPCStarted()
 {
-}*/
+    uiInterface.NotifyBlockTip.connect(&RPCNotifyBlockChange);
+}
+
+static void OnRPCStopped()
+{
+    uiInterface.NotifyBlockTip.disconnect(&RPCNotifyBlockChange);
+    RPCNotifyBlockChange(false, nullptr);
+    cvBlockChange.notify_all();
+    LogPrint(BCLog::RPC, "RPC stopped.\n");
+}
+
+static void OnRPCPreCommand(const CRPCCommand& cmd)
+{
+    // Observe safe mode
+    std::string strWarning = GetWarnings("rpc");
+    if (strWarning != "" && !GetBoolArg("-disablesafemode", DEFAULT_DISABLE_SAFEMODE) &&
+        !cmd.okSafeMode)
+        throw JSONRPCError(RPC_FORBIDDEN_BY_SAFE_MODE, std::string("Safe mode: ") + strWarning);
+}
+
+static bool AppInitServers(boost::thread_group& threadGroup)
+{
+    RPCServer::OnStarted(&OnRPCStarted);
+    RPCServer::OnStopped(&OnRPCStopped);
+    RPCServer::OnPreCommand(&OnRPCPreCommand);
+    if (!InitHTTPServer())
+        return false;
+    if (!StartRPC())
+        return false;
+    if (!StartHTTPRPC())
+        return false;
+    if (GetBoolArg("-rest", DEFAULT_REST_ENABLE) && !StartREST())
+        return false;
+    if (!StartHTTPServer())
+        return false;
+    return true;
+}
+
+bool InitRPCWarmup(boost::thread_group& threadGroup)
+{
+    MilliSleep(20000);
+    if (GetBoolArg("-server", false))
+    {
+        uiInterface.InitMessage.connect(SetRPCWarmupStatus);
+        if (!AppInitServers(threadGroup))
+            return InitError("Unable to start HTTP server. See debug log for details.");
+    }
+    return true;
+}
 
 bool InitTor(boost::thread_group& threadGroup, CScheduler& scheduler)
 {
+    if (GetBoolArg("-listenonion", DEFAULT_LISTEN_ONION))
+        StartTorControl(threadGroup, scheduler);
     return true;
 }
 
 bool ILibraryController::InitWalletFromAndroidLegacyProtoWallet(const std::string& walletFile, const std::string& oldPassword, const std::string& newPassword)
 {
-    // only exists here to keep the compiler happy, never call this on iOS
-    LogPrintf("DO NOT call ILibraryController::InitWalletFromAndroidLegacyProtoWallet on iOS\n");
+    // only exists here to keep the compiler happy, never call this on nodejs
+    LogPrintf("DO NOT call ILibraryController::InitWalletFromAndroidLegacyProtoWallet on nodejs\n");
     assert(false);
 }
 
 LegacyWalletResult ILibraryController::isValidAndroidLegacyProtoWallet(const std::string& walletFile, const std::string& oldPassword)
 {
-    // only exists here to keep the compiler happy, never call this on iOS
-    LogPrintf("DO NOT call ILibraryController::isValidAndroidLegacyProtoWallet on iOS\n");
+    // only exists here to keep the compiler happy, never call this on nodejs
+    LogPrintf("DO NOT call ILibraryController::isValidAndroidLegacyProtoWallet on nodejs\n");
     assert(false);
     return LegacyWalletResult::INVALID_OR_CORRUPT;
 }
