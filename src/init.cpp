36dafc2c4389e5f5a417369e4cb1bb996364f2a4
Malcolm MacLeod <malcolm@gulden.com>
2022-02-23 21:19:36 +0200

    QA: Remove temporary upgrade code
    This is no longer required

--- src/init.cpp
+++ src/init.cpp
@@ -1540,12 +1540,7 @@ bool AppInitMain(boost::thread_group& threadGroup, CScheduler& scheduler)
                 // If the loaded chain has a wrong genesis, bail out immediately
                 // (we're likely using a testnet datadir, or the other way around).
                 if (!mapBlockIndex.empty() && mapBlockIndex.count(chainparams.GetConsensus().hashGenesisBlock) == 0)
-                {
-                    //return InitError(_("Incorrect or no genesis block found. Wrong datadir for network?"));
-                    //fixme: (FUT) - Temporary change, revert this after a few releases
-                    fullResyncForUpgrade = true;
-                    goto fullresyncforupgrade;
-                }
+                    return InitError(_("Incorrect or no genesis block found. Wrong datadir for network?"));
 
                 // Initialize the block index (no-op if non-empty database was already loaded)
                 if (!InitBlockIndex(chainparams))
