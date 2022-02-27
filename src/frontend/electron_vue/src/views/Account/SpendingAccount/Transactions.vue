55b9e1961da885f8ab88aefa67deb39510c45a3f
Vincent van der Kroef <devlz303@gmail.com>
2022-02-27 14:20:40 +0100

    UI: use $i18n.locale in transactions list

--- src/frontend/electron_vue/src/views/Account/SpendingAccount/Transactions.vue
+++ src/frontend/electron_vue/src/views/Account/SpendingAccount/Transactions.vue
@@ -113,20 +113,7 @@ export default {
       };
       if (date.getFullYear() === new Date().getFullYear()) delete options.year;
 
-      // for now determine localization here. replace by global method
-      let language =
-        process.env.VUE_APP_I18N_LOCALE ||
-        window.navigator.language.slice(0, 2);
-      switch (language) {
-        case "nl":
-        case "en":
-          break;
-        default:
-          language = "en";
-          break;
-      }
-
-      return date.toLocaleString(language, options);
+      return date.toLocaleString(this.$i18n.locale, options);
     },
     formatTime(timestamp) {
       let date = new Date(timestamp * 1000);
