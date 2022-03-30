b0e6d2bdf00dc34729355b69fa4f52fed0b3c9d0
Vincent van der Kroef <devlz303@gmail.com>
2022-03-31 00:14:26 +0200

    fix persisting language and decimals

--- src/frontend/lite/src/store/index.js
+++ src/frontend/lite/src/store/index.js
@@ -17,8 +17,19 @@ if (process.type !== "renderer") {
 
   userSettingsPersistedState = createPersistedState({
     storage: store,
-    invertIgnored: true, // use invertIgnored because we only want to store the paths in the ignoredPaths list
-    ignoredPaths: ["app.theme"]
+    invertIgnored: true, // use invertIgnored because we only want to store the paths in the ignoredPaths list and only persist on specific commits
+    ignoredPaths: [
+      // only store the paths listed below
+      "app.theme",
+      "app.language",
+      "app.decimals"
+    ],
+    ignoredCommits: [
+      // only update persisted state on commits listed below
+      "app/SET_THEME",
+      "app/SET_LANGUAGE",
+      "app/SET_DECIMALS"
+    ]
   });
 }
 export default new Vuex.Store({
