5f1660079d9741c82ad8f33fdb5e05be80aab262
Malcolm MacLeod <malcolm@gulden.com>
2022-06-21 19:49:46 +0200

    BUILD: Update static data for release

--- src/pow/diff_old.cpp
+++ src/pow/diff_old.cpp
@@ -10,7 +10,7 @@
 #include <stdint.h>
 #include "diff_common.h"
 
-const int32_t nMaxHeight = 1568201;
+const int32_t nMaxHeight = 1605366;
 const int32_t nDiffArraySize = nMaxHeight + 1;
 const int32_t udiff[nDiffArraySize] = {
  #include "../data/static_diff_data.cpp"
