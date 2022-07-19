d399f8b3ed70e68fb900fa01ca898436ac38cc3d
Malcolm MacLeod <malcolm@gulden.com>
2022-07-19 14:43:21 +0200

    ANDROID: Update block explorer link (dactual no longer exists)

--- src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/Config.kt
+++ src/frontend/android/unity_wallet/app/src/main/java/com/gulden/unity_wallet/Config.kt
@@ -7,7 +7,7 @@ class Config {
         const val PRECISION_FULL = 8
         val DEFAULT_CURRENCY_CODE get() = getDefaultCurrencyCode()
         const val USER_AGENT = "/Gulden android:${BuildConfig.VERSION_NAME}/"
-        const val BLOCK_EXPLORER_TX_TEMPLATE = "https://www.dactual.com/transaction.php?tx=%s"
+        const val BLOCK_EXPLORER_TX_TEMPLATE = "https://gulden.chainviewer.org/tx/%s"
         const val BLOCK_EXPLORER_BLOCK_TEMPLATE = "https://blockchain.gulden.com/block/%s"
         const val PURCHASE_TEMPLATE = "https://gulden.com/purchase?receive_address=%s"
         const val AUDIBLE_NOTIFICATIONS_INTERVAL = 30 * 1000
