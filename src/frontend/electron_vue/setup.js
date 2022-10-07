a05928aade069813835b8b2cd0c056f54a0bc84b
Malcolm MacLeod <malcolm@gulden.com>
2022-10-07 15:54:23 +0200

    BUILD: LITE: Silence yarn deprecation warning

---
+++ src/frontend/electron_vue/setup.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const path = require("path");
+let src = path.join(__dirname, `holdinAPI.js.in`);
+let dst = path.join(__dirname, `holdinAPI.js`);
+fs.copyFile(src, dst, fs.constants.COPYFILE_EXCL, (err) => {})
