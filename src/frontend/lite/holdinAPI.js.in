467bcc7dbf3998c1a5f2064c2313c107c0628799
Malcolm MacLeod <malcolm@gulden.com>
2022-10-07 15:28:23 +0200

    BUILD: LITE: Copy across same setup.js mechanism that full build uses

---
+++ src/frontend/lite/holdinAPI.js.in
@@ -0,0 +1,3 @@
+const apiKey = null;
+
+export { apiKey };
---
+++ src/frontend/lite/setup.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+let src = path.join(__dirname, `holdinAPI.js.in`);
+let dst = path.join(__dirname, `holdinAPI.js`);
+fs.copyFile(src, dst, fs.constants.COPYFILE_EXCL, (err) => {})
+
+let src_filter = path.join(__dirname, `../../data/staticfiltercp`);
+let dst_filter = path.join(__dirname, `/public/staticfiltercp`);
+fs.copyFile(src_filter, dst_filter, fs.constants.COPYFILE_EXCL, (err) => {})
