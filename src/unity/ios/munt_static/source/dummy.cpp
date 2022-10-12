0504849facf3c800e49f2cbbd42fcdea99e90348
Malcolm MacLeod <malcolm@gulden.com>
2022-10-12 23:45:11 +0200

    BUILD: IOS: Improve the way we do iOS builds
    And add a few missing iOS files to Makefile

--- src/unity/ios/munt_static/source/dummy.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2018-2022 The Centure developers
-// Authored by: Willem de Jonge (willem@isnapp.nl)
-// Distributed under the Libre Chain License, see the accompanying
-// file COPYING
-
-// This is a dummy C++ file which is only present to trigger Xcode to link the required runtime
-
